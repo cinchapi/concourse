4eb324a24e888458979017bce287a1f918fab909
Jeff Nelson <jtnelson1@gmail.com>
2013-10-09 20:23:25 -0700

    more unit tests

--- concourse-server/src/test/java/org/cinchapi/concourse/server/engine/StoreTest.java
+++ concourse-server/src/test/java/org/cinchapi/concourse/server/engine/StoreTest.java
@@ -26,8 +26,10 @@ package org.cinchapi.concourse.server.engine;
 import java.util.Iterator;
 import java.util.Set;
 
+import org.cinchapi.concourse.thrift.Operator;
 import org.cinchapi.concourse.thrift.TObject;
 import org.cinchapi.concourse.time.Time;
+import org.cinchapi.concourse.util.Convert;
 import org.cinchapi.concourse.util.TestData;
 import org.junit.Assert;
 import org.junit.Rule;
@@ -62,6 +64,156 @@ public abstract class StoreTest {
 		}
 
 	};
+
+	@Test
+	public void testFindEquals() {
+		String key = TestData.getString();
+		TObject value = TestData.getTObject();
+		Set<Long> records = getRecords();
+		for (long record : records) {
+			add(key, value, record);
+		}
+		Assert.assertEquals(records, store.find(key, Operator.EQUALS, value));
+	}
+
+	@Test
+	public void testPingEmpty() {
+		Assert.assertFalse(store.ping(TestData.getLong()));
+	}
+
+	@Test
+	public void testPingAfterAdd() {
+		long record = TestData.getLong();
+		add(TestData.getString(), TestData.getTObject(), record);
+		Assert.assertTrue(store.ping(record));
+	}
+
+	@Test
+	public void testPingAfterAddAndRemove() {
+		String key = TestData.getString();
+		TObject value = TestData.getTObject();
+		long record = TestData.getLong();
+		add(key, value, record);
+		remove(key, value, record);
+		Assert.assertFalse(store.ping(record));
+	}
+
+	@Test
+	public void testDescribeEmpty() {
+		Assert.assertTrue(store.describe(TestData.getLong()).isEmpty());
+	}
+
+	@Test
+	public void testDescribeAfterAddSingle() {
+		String key = TestData.getString();
+		long record = TestData.getLong();
+		add(key, TestData.getTObject(), record);
+		Assert.assertTrue(store.describe(record).contains(key));
+	}
+
+	@Test
+	public void testDescribeAfterAddSingleWithTime() {
+		String key = TestData.getString();
+		long record = TestData.getLong();
+		long timestamp = Time.now();
+		add(key, TestData.getTObject(), record);
+		Assert.assertFalse(store.describe(record, timestamp).contains(key));
+	}
+
+	@Test
+	public void testDescribeAfterAddAndRemoveSingle() {
+		String key = TestData.getString();
+		TObject value = TestData.getTObject();
+		long record = TestData.getLong();
+		add(key, value, record);
+		remove(key, value, record);
+		Assert.assertFalse(store.describe(record).contains(key));
+	}
+
+	@Test
+	public void testDescribeAfterAddAndRemoveSingleWithTime() {
+		String key = TestData.getString();
+		TObject value = TestData.getTObject();
+		long record = TestData.getLong();
+		add(key, value, record);
+		long timestamp = Time.now();
+		remove(key, value, record);
+		Assert.assertTrue(store.describe(record, timestamp).contains(key));
+	}
+
+	@Test
+	public void testDescribeAfterAddMulti() {
+		long record = TestData.getLong();
+		Set<String> keys = getKeys();
+		for (String key : keys) {
+			add(key, TestData.getTObject(), record);
+		}
+		Assert.assertEquals(keys, store.describe(record));
+	}
+
+	@Test
+	public void testDescribeAfterAddMultiWithTime() {
+		long record = TestData.getLong();
+		Set<String> keys = getKeys();
+		for (String key : keys) {
+			add(key, TestData.getTObject(), record);
+		}
+		long timestamp = Time.now();
+		for (String key : getKeys()) {
+			add(key, TestData.getTObject(), record);
+		}
+		Assert.assertEquals(keys, store.describe(record, timestamp));
+	}
+
+	@Test
+	public void testDescribeAfterAddMultiAndRemoveMulti() {
+		long record = TestData.getLong();
+		Set<String> keys = getKeys();
+		int count = 0;
+		for (String key : keys) {
+			add(key, Convert.javaToThrift(count), record);
+			count++;
+		}
+		Iterator<String> it = keys.iterator();
+		count = 0;
+		while (it.hasNext()) {
+			String key = it.next();
+			if(TestData.getInt() % 3 == 0) {
+				it.remove();
+				remove(key, Convert.javaToThrift(count), record);
+			}
+			count++;
+		}
+		Assert.assertEquals(keys, store.describe(record));
+	}
+
+	@Test
+	public void testDescribeAfterAddMultiAndRemoveMultiWithTime() {
+		long record = TestData.getLong();
+		Set<String> keys = getKeys();
+		int count = 0;
+		for (String key : keys) {
+			add(key, Convert.javaToThrift(count), record);
+			count++;
+		}
+		Iterator<String> it = keys.iterator();
+		count = 0;
+		while (it.hasNext()) {
+			String key = it.next();
+			if(TestData.getInt() % 3 == 0) {
+				it.remove();
+				remove(key, Convert.javaToThrift(count), record);
+			}
+			count++;
+		}
+		long timestamp = Time.now();
+		count = 0;
+		for (String key : getKeys()) {
+			add(key, Convert.javaToThrift(count), record);
+			count++;
+		}
+		Assert.assertEquals(keys, store.describe(record, timestamp));
+	}
 
 	@Test
 	public void testVerifyEmpty() {
@@ -110,13 +262,13 @@ public abstract class StoreTest {
 	}
 
 	@Test
-	public void testFetchEmptyKey() {
+	public void testFetchEmpty() {
 		Assert.assertTrue(store.fetch(TestData.getString(), TestData.getLong())
 				.isEmpty());
 	}
 
 	@Test
-	public void testFetchAfterAddSingleValue() {
+	public void testFetchAfterAddSingle() {
 		String key = TestData.getString();
 		TObject value = TestData.getTObject();
 		long record = TestData.getLong();
@@ -125,7 +277,17 @@ public abstract class StoreTest {
 	}
 
 	@Test
-	public void testFetchAfterAddMultiValues() {
+	public void testFetchAfterAddSingleWithTime() {
+		String key = TestData.getString();
+		TObject value = TestData.getTObject();
+		long record = TestData.getLong();
+		long timestamp = Time.now();
+		add(key, value, record);
+		Assert.assertFalse(store.fetch(key, record, timestamp).contains(value));
+	}
+
+	@Test
+	public void testFetchAfterAddMulti() {
 		String key = TestData.getString();
 		long record = TestData.getLong();
 		Set<TObject> values = getValues();
@@ -134,9 +296,24 @@ public abstract class StoreTest {
 		}
 		Assert.assertEquals(values, store.fetch(key, record));
 	}
-	
+
 	@Test
-	public void testFetchAfterAddAndRemoveSingleValue(){
+	public void testFetchAfterAddMultiWithTime() {
+		String key = TestData.getString();
+		long record = TestData.getLong();
+		Set<TObject> values = getValues();
+		for (TObject value : values) {
+			add(key, value, record);
+		}
+		long timestamp = Time.now();
+		for (TObject value : getValues()) {
+			add(key, value, record);
+		}
+		Assert.assertEquals(values, store.fetch(key, record, timestamp));
+	}
+
+	@Test
+	public void testFetchAfterAddAndRemoveSingle() {
 		String key = TestData.getString();
 		TObject value = TestData.getTObject();
 		long record = TestData.getLong();
@@ -144,9 +321,20 @@ public abstract class StoreTest {
 		remove(key, value, record);
 		Assert.assertFalse(store.fetch(key, record).contains(value));
 	}
-	
+
 	@Test
-	public void testFetchAfterAddMultiAndRemoveMulti(){
+	public void testFetchAfterAddAndRemoveSingleWithTime() {
+		String key = TestData.getString();
+		TObject value = TestData.getTObject();
+		long record = TestData.getLong();
+		add(key, value, record);
+		long timestamp = Time.now();
+		remove(key, value, record);
+		Assert.assertTrue(store.fetch(key, record, timestamp).contains(value));
+	}
+
+	@Test
+	public void testFetchAfterAddMultiAndRemoveMulti() {
 		String key = TestData.getString();
 		long record = TestData.getLong();
 		Set<TObject> values = getValues();
@@ -154,14 +342,40 @@ public abstract class StoreTest {
 			add(key, value, record);
 		}
 		Iterator<TObject> it = values.iterator();
-		while(it.hasNext()){
+		while (it.hasNext()) {
 			TObject value = it.next();
-			if(TestData.getInt() % 3 == 0){
+			if(TestData.getInt() % 3 == 0) {
 				it.remove();
 				remove(key, value, record);
 			}
 		}
 		Assert.assertEquals(values, store.fetch(key, record));
+	}
+
+	@Test
+	public void testFetchAfterAddMultiAndRemoveMultiWithTime() {
+		String key = TestData.getString();
+		long record = TestData.getLong();
+		Set<TObject> values = getValues();
+		for (TObject value : values) {
+			add(key, value, record);
+		}
+		Iterator<TObject> it = values.iterator();
+		while (it.hasNext()) {
+			TObject value = it.next();
+			if(TestData.getInt() % 3 == 0) {
+				it.remove();
+				remove(key, value, record);
+			}
+		}
+		long timestamp = Time.now();
+		for (TObject value : getValues()) {
+			add(key, value, record);
+		}
+		for (TObject value : getValues()) {
+			remove(key, value, record);
+		}
+		Assert.assertEquals(values, store.fetch(key, record, timestamp));
 	}
 
 	/**
@@ -173,12 +387,46 @@ public abstract class StoreTest {
 		Set<TObject> values = Sets.newHashSet();
 		for (int i = 0; i < TestData.getScaleCount(); i++) {
 			TObject value = null;
-			while (value == null | values.contains(value)) {
+			while (value == null || values.contains(value)) {
 				value = TestData.getTObject();
 			}
 			values.add(value);
 		}
 		return values;
+	}
+
+	/**
+	 * Return a set of keys.
+	 * 
+	 * @return the keys.
+	 */
+	private Set<String> getKeys() {
+		Set<String> keys = Sets.newHashSet();
+		for (int i = 0; i < TestData.getScaleCount(); i++) {
+			String key = null;
+			while (key == null || keys.contains(key)) {
+				key = TestData.getString();
+			}
+			keys.add(key);
+		}
+		return keys;
+	}
+
+	/**
+	 * Return a set of primary keys.
+	 * 
+	 * @return the records
+	 */
+	private Set<Long> getRecords() {
+		Set<Long> records = Sets.newHashSet();
+		for (int i = 0; i < TestData.getScaleCount(); i++) {
+			Long record = null;
+			while (record == null || records.contains(record)) {
+				record = TestData.getLong();
+			}
+			records.add(record);
+		}
+		return records;
 	}
 
 	/**
