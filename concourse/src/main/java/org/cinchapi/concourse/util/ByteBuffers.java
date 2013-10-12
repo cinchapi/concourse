445bf284ca74a5ebec30b4b8dd142a29368cec80
Jeff Nelson <jtnelson1@gmail.com>
2013-10-12 07:35:49 -0700

    Added method to copy bytes and advance position

--- concourse/src/main/java/org/cinchapi/concourse/util/ByteBuffers.java
+++ concourse/src/main/java/org/cinchapi/concourse/util/ByteBuffers.java
@@ -30,6 +30,7 @@ import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CodingErrorAction;
 import java.nio.charset.StandardCharsets;
 
+import com.google.common.base.Preconditions;
 import com.google.common.base.Throwables;
 
 /**
@@ -110,6 +111,24 @@ public abstract class ByteBuffers {
 	 */
 	public static boolean getBoolean(ByteBuffer buffer) {
 		return buffer.get() > 0 ? true : false;
+	}
+
+	/**
+	 * Return a ByteBuffer that has a copy of {@code length} bytes from
+	 * {@code buffer} starting from the current position. This method will
+	 * advance the position of the source buffer.
+	 * 
+	 * @param buffer
+	 * @param bytes
+	 * @return a ByteBuffer that has {@code length} bytes from {@code buffer}
+	 */
+	public static ByteBuffer get(ByteBuffer buffer, int length) {
+		Preconditions
+				.checkArgument(buffer.remaining() >= length,
+						"The number of bytes remaining in the buffer cannot be less than length");
+		byte[] backingArray = new byte[length];
+		buffer.get(backingArray);
+		return ByteBuffer.wrap(backingArray);
 	}
 
 	/**
