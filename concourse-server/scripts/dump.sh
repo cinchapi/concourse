ba53fabb74f5e433273987610e798e64954548ed
Jeff Nelson <jtnelson1@gmail.com>
2013-11-07 05:52:39 -0500

    Renamed shell scripts to have a .sh extension

--- concourse-server/scripts/dump.sh
+++ concourse-server/scripts/dump.sh
@@ -2,7 +2,7 @@
 
 # This config will setup all the enviornment variables and check that
 # pthats are proper
-. "`dirname "$0"`/.env"
+. "`dirname "$0"`/.env.sh"
 
 # run the program
 exec $JAVACMD -classpath "$CLASSPATH" org.cinchapi.concourse.server.cli.DumpCli "$@"
