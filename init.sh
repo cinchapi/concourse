b7155fd710952c0e5bbbd180976d79b2a6161853
Jeff Nelson <jtnelson1@gmail.com>
2013-11-20 19:50:29 -0500

    update init

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@
 # This script will do one time initialization in the repo and delete itself
 
 printf "sonatypeUsername=\nsonatypePassword=" > gradle.properties
+git update-index --assume-unchanged $0
 rm $0
 exit 0
