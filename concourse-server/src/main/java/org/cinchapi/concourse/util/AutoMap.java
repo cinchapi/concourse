778d5814a3ece33b284772a486c973332ed5160c
Jeff Nelson <jtnelson1@gmail.com>
2013-11-24 05:03:32 -0500

    Added experimental AutoMap that dynamically loads and cleans itself up.

---
+++ concourse-server/src/main/java/org/cinchapi/concourse/util/AutoMap.java
@@ -0,0 +1,165 @@
+/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2013 Jeff Nelson, Cinchapi Software Collective
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.cinchapi.concourse.util;
+
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+import org.cinchapi.concourse.annotate.Experimental;
+
+import com.google.common.base.Function;
+
+/**
+ * An AutoMap automatically manages entry creation and removal using provided
+ * {@code loader} and {@code cleaner} functions.
+ * 
+ * @author jnelson
+ */
+@Experimental
+public abstract class AutoMap<K, V> extends AbstractMap<K, V> {
+
+    /**
+     * Return an {@link AutoMap} that is a drop in replacement for a
+     * {@link HashMap}.
+     * 
+     * @param loader
+     * @param cleaner
+     * @return the AutoMap
+     */
+    public static <K, V> AutoHashMap<K, V> newAutoHashMap(
+            Function<K, V> loader, Function<V, Boolean> cleaner) {
+        return new AutoHashMap<K, V>(loader, cleaner);
+    }
+
+    /**
+     * Return an {@link AutoMap} that is a drop in replacement for a
+     * {@link TreeMap}.
+     * 
+     * @param loader
+     * @param cleaner
+     * @return the AutoMap
+     */
+    public static <K extends Comparable<K>, V> AutoTreeMap<K, V> newAutoTreeMap(
+            Function<K, V> loader, Function<V, Boolean> cleaner) {
+        return new AutoTreeMap<K, V>(loader, cleaner);
+    }
+
+    /**
+     * Set the amount of time between each cleanup run. Changing these values
+     * won't affect existing AutoMap instances, but subsequent creations will
+     * reflect the updated values.
+     * 
+     * @param delay
+     * @param unit
+     */
+    protected static void setCleanupDelay(long delay, TimeUnit unit) { // visible
+                                                                       // for
+                                                                       // testing
+        CLEANUP_DELAY = delay;
+        CLEANUP_DELAY_UNIT = unit;
+    }
+
+    private static long CLEANUP_DELAY = 60;
+    private static TimeUnit CLEANUP_DELAY_UNIT = TimeUnit.SECONDS;
+
+    protected final Map<K, V> backingStore;
+    private final Function<K, V> loader;
+    private final Function<V, Boolean> cleaner;
+    private final ScheduledExecutorService scheduler = Executors
+            .newScheduledThreadPool(1, new ThreadFactory() {
+
+                @Override
+                public Thread newThread(Runnable r) {
+                    Thread t = new Thread(r);
+                    t.setDaemon(true);
+                    return t;
+                }
+
+            });
+
+    /**
+     * Construct a new instance.
+     * 
+     * @param backingStore
+     * @param loader
+     * @param cleaner
+     */
+    protected AutoMap(Map<K, V> backingStore, Function<K, V> loader,
+            Function<V, Boolean> cleaner) {
+        this.backingStore = backingStore;
+        this.loader = loader;
+        this.cleaner = cleaner;
+        scheduler.scheduleWithFixedDelay(clean(), 0, CLEANUP_DELAY,
+                CLEANUP_DELAY_UNIT);
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return backingStore.entrySet();
+    }
+
+    /**
+     * Return the value mapped from {@code key} if it exists, otherwise, load
+     * the value using the provided loader function.
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public V get(Object key) {
+        V value = backingStore.get(key);
+        if(value == null) {
+            value = loader.apply((K) key);
+            backingStore.put((K) key, value);
+        }
+        return value;
+    }
+
+    /**
+     * Return a Runnable command that will go through {@link #backingStore} and
+     * apply {@link #cleaner} to all the container values.
+     * 
+     * @return the Runnable command
+     */
+    private Runnable clean() {
+        return new Runnable() {
+
+            @Override
+            public void run() {
+                synchronized (backingStore) {
+                    for (Entry<K, V> entry : backingStore.entrySet()) {
+                        if(cleaner.apply(entry.getValue())) {
+                            backingStore.remove(entry.getKey());
+                        }
+                    }
+                }
+            }
+
+        };
+    }
+
+}
---
+++ concourse-server/src/main/java/org/cinchapi/concourse/util/AutoTreeMap.java
@@ -0,0 +1,179 @@
+/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2013 Jeff Nelson, Cinchapi Software Collective
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.cinchapi.concourse.util;
+
+import java.util.Comparator;
+import java.util.TreeMap;
+import java.util.NavigableMap;
+import java.util.NavigableSet;
+import java.util.SortedMap;
+
+import org.cinchapi.concourse.annotate.Experimental;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Maps;
+
+/**
+ * AutoMap with Tree based sorting. Create using
+ * {@link AutoMap#newAutoTreeMap(Function, Function)}.
+ * 
+ * @author jnelson
+ */
+@Experimental
+public class AutoTreeMap<K extends Comparable<K>, V> extends AutoMap<K, V> implements
+        NavigableMap<K, V> {
+
+    /**
+     * Construct a new instance.
+     * 
+     * @param loader
+     * @param cleaner
+     */
+    protected AutoTreeMap(Function<K, V> loader, Function<V, Boolean> cleaner) {
+        super(Maps.<K, V> newTreeMap(), loader, cleaner);
+    }
+
+    @Override
+    public Entry<K, V> ceilingEntry(K key) {
+        return ((TreeMap<K, V>) backingStore).ceilingEntry(key);
+    }
+
+    @Override
+    public K ceilingKey(K key) {
+        return ((TreeMap<K, V>) backingStore).ceilingKey(key);
+    }
+
+    @Override
+    public Comparator<? super K> comparator() {
+        return ((TreeMap<K, V>) backingStore).comparator();
+    }
+
+    @Override
+    public NavigableSet<K> descendingKeySet() {
+        return ((TreeMap<K, V>) backingStore).descendingKeySet();
+    }
+
+    @Override
+    public NavigableMap<K, V> descendingMap() {
+        return ((TreeMap<K, V>) backingStore).descendingMap();
+    }
+
+    @Override
+    public Entry<K, V> firstEntry() {
+        return ((TreeMap<K, V>) backingStore).firstEntry();
+    }
+
+    @Override
+    public K firstKey() {
+        return ((TreeMap<K, V>) backingStore).firstKey();
+    }
+
+    @Override
+    public Entry<K, V> floorEntry(K key) {
+        return ((TreeMap<K, V>) backingStore).floorEntry(key);
+    }
+
+    @Override
+    public K floorKey(K key) {
+        return ((TreeMap<K, V>) backingStore).floorKey(key);
+    }
+
+    @Override
+    public SortedMap<K, V> headMap(K toKey) {
+        return ((TreeMap<K, V>) backingStore).headMap(toKey);
+    }
+
+    @Override
+    public NavigableMap<K, V> headMap(K toKey, boolean inclusive) {
+        return ((TreeMap<K, V>) backingStore).headMap(toKey, inclusive);
+    }
+
+    @Override
+    public Entry<K, V> higherEntry(K key) {
+        return ((TreeMap<K, V>) backingStore).higherEntry(key);
+    }
+
+    @Override
+    public K higherKey(K key) {
+        return ((TreeMap<K, V>) backingStore).higherKey(key);
+    }
+
+    @Override
+    public Entry<K, V> lastEntry() {
+        return ((TreeMap<K, V>) backingStore).lastEntry();
+    }
+
+    @Override
+    public K lastKey() {
+        return ((TreeMap<K, V>) backingStore).lastKey();
+    }
+
+    @Override
+    public Entry<K, V> lowerEntry(K key) {
+        return ((TreeMap<K, V>) backingStore).lowerEntry(key);
+    }
+
+    @Override
+    public K lowerKey(K key) {
+        return ((TreeMap<K, V>) backingStore).lowerKey(key);
+    }
+
+    @Override
+    public NavigableSet<K> navigableKeySet() {
+        return ((TreeMap<K, V>) backingStore).navigableKeySet();
+    }
+
+    @Override
+    public Entry<K, V> pollFirstEntry() {
+        return ((TreeMap<K, V>) backingStore).pollFirstEntry();
+    }
+
+    @Override
+    public Entry<K, V> pollLastEntry() {
+        return ((TreeMap<K, V>) backingStore).pollLastEntry();
+    }
+
+    @Override
+    public NavigableMap<K, V> subMap(K fromKey, boolean fromInclusive, K toKey,
+            boolean toInclusive) {
+        return ((TreeMap<K, V>) backingStore).subMap(fromKey, fromInclusive,
+                toKey, toInclusive);
+    }
+
+    @Override
+    public SortedMap<K, V> subMap(K fromKey, K toKey) {
+        return ((TreeMap<K, V>) backingStore).subMap(fromKey, toKey);
+    }
+
+    @Override
+    public SortedMap<K, V> tailMap(K fromKey) {
+        return ((TreeMap<K, V>) backingStore).tailMap(fromKey);
+    }
+
+    @Override
+    public NavigableMap<K, V> tailMap(K fromKey, boolean inclusive) {
+        return ((TreeMap<K, V>) backingStore).tailMap(fromKey, inclusive);
+    }
+
+}
