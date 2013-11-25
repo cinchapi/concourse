e4dc93f08c173d5d27cdadfadb3594a48a31664e
Jeff Nelson <jtnelson1@gmail.com>
2013-11-25 14:36:22 -0500

    Changed StringTools.java to TStrings.java and fixed and issue where Limbo did not perform searches using the same semantics as the Database

---
+++ concourse-server/src/main/java/org/cinchapi/concourse/util/TStrings.java
@@ -0,0 +1,105 @@
+/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2013 Jeff Nelson, Cinchapi Software Collective
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.cinchapi.concourse.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.cinchapi.concourse.server.GlobalState;
+
+/**
+ * A collection of {@link String} related tools.
+ * 
+ * @author jnelson
+ */
+public final class TStrings {
+
+    /**
+     * Return {@code true} if {@code haystack} is an <strong>infix
+     * search match</strong> for {@code needle}. If {@code haystack} is an infix
+     * search match, it means that it contains a sequence of terms where each
+     * term or a substring of the term matches the term in the same relative
+     * position in {@code needle}.
+     * <p>
+     * <ul>
+     * <li><em>foo bar</em> (haystack) <strong>IS</strong> a match for
+     * <em>foo bar</em> (needle)</li>
+     * <li><em>foo bar</em> (haystack) <strong>IS</strong> a match for
+     * <em>f bar</em> (needle)</li>
+     * <li><em>foo bar</em> (haystack) <strong>IS</strong> a match for
+     * <em>oo a</em> (needle)</li>
+     * <li><em>f b</em> (haystack) <strong>IS</strong> a match for
+     * <em>f bar</em> (needle)</li>
+     * <li><em>barfoobar foobarfoo</em> (haystack) <strong>IS</strong> a match
+     * for <em>f bar</em> (needle)</li>
+     * </ul>
+     * </p>
+     * 
+     * @param needle
+     * @param haystack
+     * @return {@code true} if {@code haystack} is an infix search match for
+     *         {@code needle}.
+     */
+    public static boolean isInfixSearchMatch(String needle, String haystack) {
+        StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        for (String term : needle.split(" ")) {
+            if(!first) {
+                sb.append(REGEX_SINGLE_WHITESPACE);
+            }
+            sb.append(REGEX_ZERO_OR_MORE_NON_WHITESPACE_CHARS);
+            sb.append(term);
+            sb.append(REGEX_ZERO_OR_MORE_NON_WHITESPACE_CHARS);
+            first = false;
+        }
+        Pattern pattern = Pattern.compile(sb.toString());
+        Matcher matcher = pattern.matcher(haystack);
+        return matcher.find();
+    }
+
+    /**
+     * Return a copy of {@code string} with all of the stopwords removed. This
+     * method depends on the stopwords defined in {@link GlobalState#STOPWORDS}.
+     * 
+     * @param string
+     * @return A copy of {@code string} without stopwords
+     */
+    public static String stripStopWords(String string) {
+        String[] toks = string.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (String tok : toks) {
+            if(!GlobalState.STOPWORDS.contains(tok)) {
+                sb.append(tok);
+                sb.append(" ");
+            }
+        }
+        return sb.toString().trim();
+    }
+
+    private static final String REGEX_ZERO_OR_MORE_NON_WHITESPACE_CHARS = "[^\\s]*";
+    private static final String REGEX_SINGLE_WHITESPACE = "[\\s]";
+
+    private TStrings() {/* utility class */}
+
+}
--- concourse-server/src/test/java/org/cinchapi/concourse/server/storage/StoreTest.java
+++ concourse-server/src/test/java/org/cinchapi/concourse/server/storage/StoreTest.java
@@ -37,7 +37,7 @@ import org.cinchapi.concourse.thrift.TObject;
 import org.cinchapi.concourse.time.Time;
 import org.cinchapi.concourse.util.Convert;
 import org.cinchapi.concourse.util.Numbers;
-import org.cinchapi.concourse.util.StringTools;
+import org.cinchapi.concourse.util.TStrings;
 import org.cinchapi.concourse.util.TestData;
 import org.junit.Assert;
 import org.junit.Rule;
@@ -471,6 +471,20 @@ public abstract class StoreTest extends ConcourseBaseTest {
 
     @Test
     @Theory
+    public void testSearchReproA(SearchType type) {
+        String query = Variables.register("query", "tc e");
+        String key = Variables.register("key", "2 6f0wzcw2ixa   dcf sa");
+        Set<Long> records = setupSearchTest(
+                key,
+                query,
+                type,
+                Lists.newArrayList(6631928579149921621L),
+                Lists.newArrayList("qi2sqa06xhn5quxbdasrtjsrzucbmo24fc4u78iv4rtc1ea7dnas74uxadvrf"));
+        Assert.assertEquals(records, store.search(key, query));
+    }
+
+    @Test
+    @Theory
     public void testSearchReproCON_2(SearchType type) {
         String query = Variables.register("query", "k i");
         String key = Variables.register("key", "oq99f7u7vizpob4o");
@@ -707,12 +721,14 @@ public abstract class StoreTest extends ConcourseBaseTest {
                         .size() == otherSource.size()));
         Set<Long> records = Sets.newHashSet();
         recordSource = recordSource == null ? getRecords() : recordSource;
-        if(!Strings.isNullOrEmpty(StringTools.stripStopWords(query))) {
+        if(!Strings.isNullOrEmpty(TStrings.stripStopWords(query))) {
             int i = 0;
             for (long record : recordSource) {
                 if(otherSource != null) {
                     String other = otherSource.get(i);
-                    boolean matches = other.contains(query);
+                    boolean matches = TStrings.isInfixSearchMatch(
+                            TStrings.stripStopWords(query),
+                            TStrings.stripStopWords(other));
                     SearchTestItem sti = Variables.register("sti_" + record,
                             new SearchTestItem(key,
                                     Convert.javaToThrift(other), record, query,
@@ -726,9 +742,9 @@ public abstract class StoreTest extends ConcourseBaseTest {
                     String other = null;
                     while (other == null
                             || other.equals(query)
-                            || query.contains(other)
-                            || other.contains(query)
-                            || Strings.isNullOrEmpty(StringTools
+                            || TStrings.isInfixSearchMatch(query, other)
+                            || TStrings.isInfixSearchMatch(other, query)
+                            || Strings.isNullOrEmpty(TStrings
                                     .stripStopWords(other))) {
                         other = TestData.getString();
                     }
