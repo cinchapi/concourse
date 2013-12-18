41d0f7b8be203bf4a02b62bd426110e14b0e7d8b
Jeff Nelson <jtnelson1@gmail.com>
2013-12-18 15:29:15 -0500

    use single package for upgrade and install

--- concourse-server/.installer.sh
+++ concourse-server/.installer.sh
@@ -14,19 +14,7 @@ VERSION=$1
 DISTS="build/distributions"
 cd $DISTS
 unzip concourse-server*zip
-cd -
-
-########################################################################
-############################ INSTALLER #################################
-########################################################################
-
-INSTALLER="concourse-server-$VERSION-install.bin"
-../makeself/makeself.sh --notemp $DISTS/concourse-server $INSTALLER "Concourse Server"
-mv $INSTALLER $DISTS
-
-########################################################################
-############################# UPGRADER #################################
-########################################################################
+cd - >> /dev/null
 
 SCRIPT_NAME=".update"
 SCRIPT="$DISTS/concourse-server/$SCRIPT_NAME"
@@ -34,37 +22,54 @@ SCRIPT="$DISTS/concourse-server/$SCRIPT_NAME"
 # We dynamically create an "update" script that copies certain files from
 # the new distribution to the current install directory. Afterwards, the
 # update script will start the server and run the upgrade task
-
-# --- copy files
 echo "#!/usr/bin/env bash" >> $SCRIPT
+
+# --- check if we should upgrade by detecting if the directory above is an
+# --- installation diretory
+echo "files=\$(ls ../lib/concourse*.jar 2> /dev/null | wc -l)" >> $SCRIPT
+
+# --- copy upgrade files
+echo "if [ \$files -gt 0 ]; then" >> $SCRIPT #start upgrade
+echo "echo 'Upgrading Concourse Server.........................................................................'" >> $SCRIPT
 echo "cp -R lib/ ../lib/" >> $SCRIPT
 echo "cp -R bin/ ../bin/" >> $SCRIPT
 echo "cp -R licenses/ ../licenses/" >> $SCRIPT
 # TODO: Copy config files???
-echo "cd .." >> $SCRIPT
 
 # --- run upgrade task
+echo "cd .." >> $SCRIPT
 # TODO exec bin/start
 # TODO exec bin/upgrade
 # TODO exec bin/stop
 
-# --- delete upgrade working files
+echo "cd - >> /dev/null" >> $SCRIPT
+echo "fi" >> $SCRIPT #end upgrade
+
+# -- delete the update file and installer
+echo "rm $SCRIPT_NAME" >> $SCRIPT
+echo "cd .." >> $SCRIPT
+echo "rm concourse-server*bin" >> $SCRIPT
+
+# --- delete upgrade directory
+echo "if [ \$files -gt 0 ]; then" >> $SCRIPT
 echo "rm -r concourse-server" >> $SCRIPT
-echo "rm concourse-server*upgrade" >> $SCRIPT
+echo "fi" >> $SCRIPT
+
 echo "exit 0" >> $SCRIPT
 
-# Make upgrade script executable
+# Make installer executable
 chmod +x $SCRIPT
 
-UPGRADER="concourse-server-$VERSION-upgrade.bin"
-../makeself/makeself.sh --notemp $DISTS/concourse-server $UPGRADER "Concourse Server" ./$SCRIPT_NAME
-mv $UPGRADER $DISTS
+INSTALLER="concourse-server-$VERSION.bin"
+../makeself/makeself.sh --notemp $DISTS/concourse-server $INSTALLER "Concourse Server" ./$SCRIPT_NAME
+chmod +x $INSTALLER
+mv $INSTALLER $DISTS
 
 ########################################################################
 ############################# #CLEANUP #################################
 ########################################################################
 cd $DISTS
 rm -rf concourse-server
-cd -
+cd - >> /dev/null
 
 exit 0
