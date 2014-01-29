4bafe400fbe6b8a9d30ba7088dc0aa09093f336c
Jeff Nelson <jtnelson1@gmail.com>
2014-01-28 21:27:05 -0500

    basic transaction workflow tests

---
+++ concourse-testing/src/test/java/org/cinchapi/concourse/TransactionWorkflowTest.java
@@ -0,0 +1,76 @@
+/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2014 Jeff Nelson, Cinchapi Software Collective
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.cinchapi.concourse;
+
+import org.cinchapi.concourse.util.TestData;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * ETE tests for transaction related workflows.
+ * 
+ * @author jnelson
+ */
+public class TransactionWorkflowTest extends ConcourseIntegrationTest {
+
+    /**
+     * A second client that is connected to the server on behalf a different,
+     * non-admin user.
+     */
+    private Concourse client2;
+
+    @Override
+    protected void beforeEachTest() {
+        String username = TestData.getString();
+        String password = TestData.getString();
+        grantAccess(username, password);
+        client2 = Concourse.connect(SERVER_HOST, SERVER_PORT, username,
+                password);
+    }
+
+    @Test
+    public void testIsolation() {
+        String key = TestData.getString();
+        Object value = TestData.getObject();
+        long record = TestData.getLong();
+        client.stage();
+        client2.stage();
+        client.add(key, value, record);
+        Assert.assertTrue(client.verify(key, value, record));
+        Assert.assertFalse(client2.verify(key, value, record));
+    }
+
+    @Test
+    public void testDurabilityAfterServerRestart() {
+        String key = TestData.getString();
+        Object value = TestData.getObject();
+        long record = TestData.getLong();
+        client.stage();
+        client.stage();
+        client.add(key, value, record);
+        restartServer();
+        Assert.assertTrue(client.verify(key, value, record));
+    }
+
+}
