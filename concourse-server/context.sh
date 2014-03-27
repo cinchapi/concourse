0c23299ccc082281cd0931b8a8aa5fd9308c79d6
Jeff Nelson <jtnelson1@gmail.com>
2014-03-27 13:41:09 -0400

    don't display rm erros for real

--- concourse-server/context.sh
+++ concourse-server/context.sh
@@ -2,6 +2,6 @@
 # Script to print context info in the project
 
 FILE="context.txt";
-rm $FILE >> /dev/null
+rm $FILE 2>/dev/null
 cat <(echo "Last Commit:" & git rev-parse HEAD) <(git status) <(git diff ../*) > $FILE
 exit 0
