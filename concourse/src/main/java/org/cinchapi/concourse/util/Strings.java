b0a78acb269569945a4d12e0b4676436f52d40ad
Chandresh Pancholi <chandresh.pancholi@flipkart.com>
2014-05-22 21:20:55 +0530

    Adding javadoc in String util class

--- concourse/src/main/java/org/cinchapi/concourse/util/Strings.java
+++ concourse/src/main/java/org/cinchapi/concourse/util/Strings.java
@@ -1,13 +1,20 @@
 package org.cinchapi.concourse.util;
 
+
 import java.lang.reflect.Field;
 import org.cinchapi.concourse.annotate.UtilityClass;
-
+ /**
+ * This file contains String related utility functions
+ **/
 
 @UtilityClass
 public class Strings {
+
+
     /**
      * Use of this method is to nullify the strings.
+     * Warning: this method will modify the string in place which departs from the normal contract that strings are immutable
+     * and will also make the string unusable
      **/
     public static void nullify(String string){
         try{
@@ -19,7 +26,7 @@ public class Strings {
             }
         }
         catch(Exception e){
-            System.out.println(e.getStackTrace());
+            throw Throwables.propogate(e);
         }
 
     }
