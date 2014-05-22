d50ed5a58dbfb0f5baaad1306a9b7129829767e2
Chandresh Pancholi <chandresh.pancholi@flipkart.com>
2014-05-22 20:47:37 +0530

    "Bug CON-64" refactoring of code

--- concourse-server/src/main/java/org/cinchapi/concourse/server/ConcourseServer.java
+++ concourse-server/src/main/java/org/cinchapi/concourse/server/ConcourseServer.java
@@ -280,7 +280,8 @@ public class ConcourseServer implements
                     .containsKey(transaction)) || transaction == null);
             return transaction != null ? transactions.get(transaction).add(key,
                     value, record) : engine.add(key, value, record);
-        }else{
+        }
+        else{
             return false;
         }
     }
@@ -457,7 +458,8 @@ public class ConcourseServer implements
                     .containsKey(transaction)) || transaction == null);
             return transaction != null ? transactions.get(transaction).remove(key,
                     value, record) : engine.remove(key, value, record);
-        }else{
+        }
+        else{
             return false;
         }
     }
