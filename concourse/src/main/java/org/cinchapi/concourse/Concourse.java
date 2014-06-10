7ceb839a07ad5350847e0d2588e8d10782c52143
Jeff Nelson <jtnelson1@gmail.com>
2014-06-09 21:54:06 -0400

    display actual key name in browse result set

--- concourse/src/main/java/org/cinchapi/concourse/Concourse.java
+++ concourse/src/main/java/org/cinchapi/concourse/Concourse.java
@@ -1251,7 +1251,7 @@ public abstract class Concourse {
                 @Override
                 public Map<Object, Set<Long>> call() throws Exception {
                     Map<Object, Set<Long>> data = TLinkedHashMap
-                            .newTLinkedHashMap("Value", "Records");
+                            .newTLinkedHashMap(key, "Records");
                     for (Entry<TObject, Set<Long>> entry : client.browse1(key,
                             timestamp.getMicros(), creds, transaction)
                             .entrySet()) {
