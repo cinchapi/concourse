8f8fcf199fb4e4a1ea8166dd6accc445b9adda3d
Jeff Nelson <jtnelson1@gmail.com>
2014-09-08 19:34:25 -0400

    CON-170: dont display stopwatch info for empty line input

--- concourse-shell/src/main/java/org/cinchapi/concourse/shell/ConcourseShell.java
+++ concourse-shell/src/main/java/org/cinchapi/concourse/shell/ConcourseShell.java
@@ -141,6 +141,9 @@ public final class ConcourseShell {
                     System.err.println("Cannot complete command because "
                             + "it contains an illegal character sequence.");
                 }
+                else if(Strings.isNullOrEmpty(line)) { // CON-170
+                    continue;
+                }
                 else {
                     watch.reset().start();
                     Object value = null;
