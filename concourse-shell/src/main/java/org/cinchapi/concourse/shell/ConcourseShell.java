98644b7752742dab71621134299b73b0782bd970
Jeff Nelson <jtnelson1@gmail.com>
2014-09-24 14:06:42 -0400

    change error exit status

--- concourse-shell/src/main/java/org/cinchapi/concourse/shell/ConcourseShell.java
+++ concourse-shell/src/main/java/org/cinchapi/concourse/shell/ConcourseShell.java
@@ -218,7 +218,7 @@ public final class ConcourseShell {
      */
     private static void die(String message) {
         System.err.println("ERROR: " + message);
-        System.exit(127);
+        System.exit(1);
     }
 
     /**
