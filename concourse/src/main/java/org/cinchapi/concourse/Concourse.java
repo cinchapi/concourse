b840430e9058d3a84080669279f969b9e7bb8496
Jeff Nelson <jtnelson1@gmail.com>
2014-10-19 12:03:27 -0400

    Revert "use different thrift protocols and transports"
    This reverts commit 0d9a9063008c1141840757e80113ff253ea3d9ae.

--- concourse/src/main/java/org/cinchapi/concourse/Concourse.java
+++ concourse/src/main/java/org/cinchapi/concourse/Concourse.java
@@ -31,13 +31,11 @@ import java.util.Set;
 import java.util.concurrent.Callable;
 
 import javax.annotation.Nullable;
-import javax.annotation.concurrent.NotThreadSafe;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.thrift.TException;
-import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
-import org.apache.thrift.transport.TFastFramedTransport;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
@@ -69,19 +67,18 @@ import com.google.common.collect.Lists;
 /**
  * <p>
  * Concourse is a schemaless and distributed version control database with
- * automatic indexing, acid transactions and full-text search. Concourse
- * provides a more intuitive approach to data management that is easy to deploy,
- * access and scale with minimal tuning while also maintaining the referential
- * integrity and ACID characteristics of traditional database systems.
+ * optimistic availability, serializable transactions and full-text search.
+ * Concourse provides a more intuitive approach to data management that is easy
+ * to deploy, access and scale with minimal tuning while also maintaining the
+ * referential integrity and ACID characteristics of traditional database
+ * systems.
  * </p>
  * <h2>Data Model</h2>
  * <p>
- * The Concourse data model is lightweight and flexible. Unlike other databases,
- * Concourse is completely schemaless and does not hold data in tables or
- * collections. Instead, Concourse is simply a distributed graph of records.
- * Each record has multiple keys. And each key has one or more distinct values.
- * Like any graph, you can link records to one another. And the structure of one
- * record does not affect the structure of another.
+ * The Concourse data model is lightweight and flexible which enables it to
+ * support any kind of data at very large scales. Concourse trades unnecessary
+ * structural notions of schemas, tables and indexes for a more natural modeling
+ * of data based solely on the following concepts:
  * </p>
  * <p>
  * <ul>
@@ -93,7 +90,7 @@ import com.google.common.collect.Lists;
  * different {@code keys}, and the {@code keys} in one {@code record} do not
  * affect those in another {@code record}.
  * <li><strong>Value</strong> &mdash; A dynamically typed quantity that is
- * associated with a {@code key} in a {@code record}.
+ * mapped from a {@code key} in a {@code record}.
  * </ul>
  * </p>
  * <h4>Data Types</h4>
@@ -119,17 +116,9 @@ import com.google.common.collect.Lists;
  * {@link #commit()} and {@link #abort()} methods.
  * 
  * </p>
- * <h2>Thread Safety</h2>
- * <p>
- * You should <strong>not</strong> use the same client connection in multiple
- * threads. If you need to interact with Concourse using multiple threads, you
- * should create a separate connection for each thread or use a
- * {@link ConnectionPool}.
- * </p>
  * 
  * @author jnelson
  */
-@NotThreadSafe
 public abstract class Concourse implements AutoCloseable {
 
     /**
@@ -1261,11 +1250,10 @@ public abstract class Concourse implements AutoCloseable {
             this.username = ClientSecurity.encrypt(username);
             this.password = ClientSecurity.encrypt(password);
             this.environment = environment;
-            final TTransport transport = new TFastFramedTransport(new TSocket(
-                    host, port));
+            final TTransport transport = new TSocket(host, port);
             try {
                 transport.open();
-                TProtocol protocol = new TCompactProtocol(transport);
+                TProtocol protocol = new TBinaryProtocol(transport);
                 client = new ConcourseService.Client(protocol);
                 authenticate();
                 Runtime.getRuntime().addShutdownHook(new Thread("shutdown") {
@@ -1343,6 +1331,7 @@ public abstract class Concourse implements AutoCloseable {
                             .isBlank((String) value)))) { // CON-21
                 return execute(new Callable<Boolean>() {
 
+                    @Override
                     public Boolean call() throws Exception {
                         return client.add(key, Convert.javaToThrift(value),
                                 record, creds, transaction, environment);
