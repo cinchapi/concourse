e5d90d0c33a9c2abee90873a77a4a40388f75fdf
remiemalik <rmalik@linprofs.com>
2015-01-04 16:48:06 +0100

    Test the LIKE and NOT_LIKE operators with more detail

--- concourse-integration-tests/src/test/java/org/cinchapi/concourse/OperatorAliasTest.java
+++ concourse-integration-tests/src/test/java/org/cinchapi/concourse/OperatorAliasTest.java
@@ -1,7 +1,6 @@
 package org.cinchapi.concourse;
 
 import org.cinchapi.concourse.thrift.Operator;
-import org.cinchapi.concourse.util.TestData;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,30 +12,45 @@ import org.junit.Test;
  */
 public class OperatorAliasTest extends ConcourseIntegrationTest {
 
-	@Test
-	public void testLikeAlias() {
-		String key = TestData.getString();
-		int record = TestData.getScaleCount();
-		client.add(key, 1, record);
-		Assert.assertEquals(client.find(key, Operator.LIKE, record),
-				client.find(key, Operator.REGEX, record));
-	}
+    @Test
+    public void testLikeAlias() {
+        client.add("cmd", "cd /home/applications/eclipse/", 1);
+        client.add("cmd", "cd /home/desktop", 2);
+        client.add("cmd", "vim /var/www/html/index.html", 3);
+        client.add("cmd", "cat /home/notes.txt", 4);
+        client.add("cmd", "find / -name home", 5);
+        client.add("cmd", "ls -la /HOME/", 6);
+        client.add("cmd", "mkdir home_directory", 7);
+        client.add("cmd", "cp /var/www/home123 /opt/home", 8);
+        Assert.assertEquals(client.find("cmd", Operator.LIKE, "%/home/%"),
+                client.find("cmd", Operator.REGEX, "%/home/%"));
+    }
 
-	@Test
-	public void testNotLikeAlias() {
-		String key = TestData.getString();
-		int record = TestData.getScaleCount();
-		client.add(key, 1, record);
-		Assert.assertEquals(client.find(key, Operator.NOT_LIKE, record),
-				client.find(key, Operator.NOT_REGEX, record));
-	}
+    @Test
+    public void testNotLikeAlias() {
+        client.add("cmd", "cd /home/applications/eclipse/", 1);
+        client.add("cmd", "cd /home/desktop", 2);
+        client.add("cmd", "vim /var/www/html/index.html", 3);
+        client.add("cmd", "cat /home/notes.txt", 4);
+        client.add("cmd", "find / -name home", 5);
+        client.add("cmd", "ls -la /HOME/", 6);
+        client.add("cmd", "mkdir home_directory", 7);
+        client.add("cmd", "cp /var/www/home123 /opt/home", 8);
+        Assert.assertEquals(client.find("cmd", Operator.NOT_LIKE, "%/home/%"),
+                client.find("cmd", Operator.NOT_REGEX, "%/home/%"));
+    }
 
-	@Test
-	public void testLinkToAlias() {
-		String key = TestData.getString();
-		int record = TestData.getScaleCount();
-		client.add(key, 1, record);
-		Assert.assertEquals(client.find(key, Operator.LINKS_TO, record),
-				client.find(key, Operator.EQUALS, record));
-	}
+    @Test
+    public void testLinkToAlias() {
+        client.add("cmd", "cd /home/applications/eclipse/", 1);
+        client.add("cmd", "cd /home/desktop", 2);
+        client.add("cmd", "vim /var/www/html/index.html", 3);
+        client.add("cmd", "cat /home/notes.txt", 4);
+        client.add("cmd", "find / -name home", 5);
+        client.add("cmd", "ls -la /HOME/", 6);
+        client.add("cmd", "mkdir home_directory", 7);
+        client.add("cmd", "cp /var/www/home123 /opt/home", 8);
+        Assert.assertEquals(client.find("cmd", Operator.LINKS_TO, "/home/"),
+                client.find("cmd", Operator.EQUALS, "/home/"));
+    }
 }
