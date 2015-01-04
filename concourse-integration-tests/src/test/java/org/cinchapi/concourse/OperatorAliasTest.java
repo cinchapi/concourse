eed2a6c1dba559afd451cba7e5b3336d39b71df4
Jeff Nelson <jtnelson1@gmail.com>
2015-01-04 11:10:44 -0500

    fix linksto test

--- concourse-integration-tests/src/test/java/org/cinchapi/concourse/OperatorAliasTest.java
+++ concourse-integration-tests/src/test/java/org/cinchapi/concourse/OperatorAliasTest.java
@@ -42,15 +42,11 @@ public class OperatorAliasTest extends ConcourseIntegrationTest {
 
     @Test
     public void testLinkToAlias() {
-        client.add("cmd", "cd /home/applications/eclipse/", 1);
-        client.add("cmd", "cd /home/desktop", 2);
-        client.add("cmd", "vim /var/www/html/index.html", 3);
-        client.add("cmd", "cat /home/notes.txt", 4);
-        client.add("cmd", "find / -name home", 5);
-        client.add("cmd", "ls -la /HOME/", 6);
-        client.add("cmd", "mkdir home_directory", 7);
-        client.add("cmd", "cp /var/www/home123 /opt/home", 8);
-        Assert.assertEquals(client.find("cmd", Operator.LINKS_TO, "/home/"),
-                client.find("cmd", Operator.EQUALS, "/home/"));
+        client.link("foo", 1, 2);
+        client.link("foo", 3, 2);
+        client.link("foo", 4, 2);
+        client.link("foo", 5, 2);
+        Assert.assertEquals(client.find("cmd", Operator.LINKS_TO, 2),
+                client.find("cmd", Operator.EQUALS, Link.to(2)));
     }
 }
