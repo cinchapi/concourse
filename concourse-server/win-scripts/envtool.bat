3abba09c3d17604bf4a13dcd18f37df7947f5d2f
dubex <prakashdubey70@gmail.com>
2015-01-11 22:55:09 +0530

    changed bash script to batch file

---
+++ concourse-server/win-scripts/envtool.bat
@@ -0,0 +1,7 @@
+@if "%DEBUG%" == "" @echo off
+@rem This config will setup all the enviornment variables and check that
+@rem paths are proper
+call .env.bat %*
+
+@rem run the program
+"%JAVA_EXE%" -classpath "%CLASSPATH%"org.cinchapi.concourse.server.cli.EnvToolCli %CMD_LINE_ARGS%
