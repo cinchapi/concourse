8069343d47b381bd36ea00fd8f14611530c13bf0
Jeff Nelson <jtnelson1@gmail.com>
2015-01-23 13:25:06 -0500

    add comparator to sort range tokens in an approximate order based on their value

--- concourse-server/src/main/java/org/cinchapi/concourse/server/concurrent/RangeTokens.java
+++ concourse-server/src/main/java/org/cinchapi/concourse/server/concurrent/RangeTokens.java
@@ -23,11 +23,14 @@ package org.cinchapi.concourse.server.concurrent;
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+import java.util.Comparator;
 import java.util.List;
+
 import org.cinchapi.common.util.Range;
 import org.cinchapi.concourse.server.model.Value;
 import org.cinchapi.concourse.thrift.Operator;
 
+import com.google.common.collect.ComparisonChain;
 import com.google.common.collect.Lists;
 
 /**
@@ -85,5 +88,59 @@ public final class RangeTokens {
         }
         return ranges;
     }
+
+    /**
+     * A comparator that gives an approximate sorting or range tokens based on
+     * their values. This comparator rarely says range tokens are equal (e.g.
+     * equality is pretty much limited to cases when the tokens have the same
+     * number of values, each of which are equal. Generally speaking, this
+     * comparator will seek to sort RangeTokens in the way that they would
+     * appear on a number line.
+     * <p>
+     * <strong>WARNING:</strong> This comparator intentionally considers range
+     * tokens that represent an EQUAL read (e.g. key = foo) to be the same as
+     * range tokens that represent a matching write (e.g. key AS foo). Ensure
+     * that this feature is acceptable for your use case before sorting with
+     * this comparator.
+     * </p>
+     * <p>
+     * <strong>NOTE:</strong> This comparator does not take the {@code key}
+     * component of the RangeToken into account!!!
+     * </p>
+     */
+    public static Comparator<RangeToken> APPROX_VALUE_COMPARATOR = new Comparator<RangeToken>() {
+
+        @Override
+        public int compare(RangeToken arg0, RangeToken arg1) {
+            List<Range<Value>> ranges0 = Lists
+                    .newArrayList(convertToRange(arg0));
+            List<Range<Value>> ranges1 = Lists
+                    .newArrayList(convertToRange(arg1));
+            if(ranges0.size() == ranges1.size()) {
+                ComparisonChain chain = ComparisonChain.start();
+                for (int i = 0; i < ranges0.size(); i++) {
+                    chain = chain.compare(ranges0.get(i), ranges1.get(i));
+                }
+                return chain.result();
+            }
+            else if(ranges0.size() > 1) {
+                for (Range<Value> range : ranges0) {
+                    if(range.compareTo(ranges1.get(0)) < 0) {
+                        return -1;
+                    }
+                }
+                return 1;
+            }
+            else { // ranges1.size() > 1
+                for (Range<Value> range : ranges1) {
+                    if(range.compareTo(ranges0.get(0)) < 0) {
+                        return -1;
+                    }
+                }
+                return 1;
+            }
+        }
+
+    };
 
 }
