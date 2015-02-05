62e54ca7dc42a26a9583afb5e33af7c6fe9b1c00
Jeff Nelson <jtnelson1@gmail.com>
2015-02-05 09:31:21 -0500

    add unit test that repros bug

--- concourse-server/src/test/java/org/cinchapi/concourse/server/storage/EngineTest.java
+++ concourse-server/src/test/java/org/cinchapi/concourse/server/storage/EngineTest.java
@@ -26,6 +26,7 @@ package org.cinchapi.concourse.server.storage;
 import java.io.File;
 import java.lang.reflect.Method;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -237,6 +238,56 @@ public class EngineTest extends BufferedStoreTest {
             Engine.BUFFER_TRANSPORT_THREAD_HUNG_DETECTION_FREQUENCY_IN_MILLISECONDS = frequency;
             Engine.BUFFER_TRANSPORT_THREAD_HUNG_DETECTION_THRESOLD_IN_MILLISECONDS = threshold;
         }
+    }
+
+    @Test
+    public void reproCON_239A() throws InterruptedException {
+        final Engine engine = (Engine) store;
+        int count = TestData.getScaleCount();
+        for (int i = 0; i < count; i++) {
+            engine.add(Long.toString(Time.now()), Convert.javaToThrift(i), 1);
+        }
+        final AtomicBoolean done = new AtomicBoolean(false);
+        final AtomicBoolean go = new AtomicBoolean(false);
+        Thread write = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (!go.get()) {
+                    continue; // ensure read goes first
+                }
+                while (!done.get()) {
+                    if(!done.get()) {
+                        engine.add(Long.toString(Time.now()),
+                                Convert.javaToThrift("a"), 1);
+                    }
+                }
+            }
+
+        });
+        final AtomicBoolean succeeded = new AtomicBoolean(true);
+        Thread read = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                go.set(true);
+                Map<String, Set<TObject>> data = engine.browse(1);
+                done.set(true);
+                Map<String, Set<TObject>> data1 = engine.browse(1);
+                Variables.register("data_size", data.size());
+                Variables.register("data1_size", data1.size());
+                succeeded.set(data.size() == data1.size()
+                        || data.size() == data1.size() - 1);
+            }
+
+        });
+
+        read.start();
+        write.start();
+        read.join();
+        write.join();
+        Assert.assertTrue(succeeded.get());
+
     }
 
     @Override
