7a30b7f14f5121009d875b12356c0e09e0a69a94
Jeff Nelson <jtnelson1@gmail.com>
2015-02-10 09:15:07 -0500

    RangeReadWriteLock is not implemented to properly take advantage of decoration
    Conflicts:
    	concourse-server/src/main/java/org/cinchapi/concourse/server/concurrent/RangeReadWriteLock.java

--- concourse-server/src/main/java/org/cinchapi/concourse/server/concurrent/RangeReadWriteLock.java
+++ concourse-server/src/main/java/org/cinchapi/concourse/server/concurrent/RangeReadWriteLock.java
@@ -50,8 +50,8 @@ final class RangeReadWriteLock extends ReferenceCountingLock {
     /**
      * Construct a new instance.
      * 
+     * @param rangeLockService
      * @param token
-     * @param rangeLockService TODO
      */
     public RangeReadWriteLock(RangeLockService rangeLockService,
             RangeToken token) {
@@ -87,9 +87,16 @@ final class RangeReadWriteLock extends ReferenceCountingLock {
     }
 
     @Override
-    public String toString() {
-        return super.toString() + "[id = " + System.identityHashCode(this)
-                + "]";
+    public void beforeReadLock() {
+        while (rangeLockService.isRangeBlocked(LockType.READ, token)) {
+            Thread.yield();
+            continue;
+        }
+    }
+
+    @Override
+    public boolean tryBeforeReadLock() {
+        return !rangeLockService.isRangeBlocked(LockType.READ, token);
     }
 
     @Override
@@ -116,6 +123,19 @@ final class RangeReadWriteLock extends ReferenceCountingLock {
             }
 
         };
+    }
+
+    @Override
+    public void beforeWriteLock() {
+        while (rangeLockService.isRangeBlocked(LockType.WRITE, token)) {
+            Thread.yield();
+            continue;
+        }
+    }
+
+    @Override
+    public boolean tryBeforeWriteLock() {
+        return !rangeLockService.isRangeBlocked(LockType.WRITE, token);
     }
 
 }
