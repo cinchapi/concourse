45faa5e90916758ae9630b8ee6051e6f6271c9ae
dubex <prakashdubey70@gmail.com>
2015-02-12 00:10:15 +0530

    fixed transaction memory leak bug

--- concourse-server/src/main/java/org/cinchapi/concourse/server/ConcourseServer.java
+++ concourse-server/src/main/java/org/cinchapi/concourse/server/ConcourseServer.java
@@ -407,8 +407,7 @@ public class ConcourseServer implements
         checkAccess(creds, transaction);
         try {
             Compoundable store = getStore(transaction, env);
-            Map<Long, Set<TObject>> result = PrettyLinkedHashMap
-                    .newPrettyLinkedHashMap();
+            Map<Long, Set<TObject>> result = PrettyLinkedHashMap.newPrettyLinkedHashMap();
             Map<Long, String> history = store.audit(key, record);
             for (Long timestamp : history.keySet()) {
                 Set<TObject> values = store.fetch(key, record, timestamp);
