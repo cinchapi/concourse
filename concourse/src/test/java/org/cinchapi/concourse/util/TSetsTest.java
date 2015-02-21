4d617b851f784b61cc70fb61e7fac32b1591df2f
Jeff Nelson <jtnelson1@gmail.com>
2015-02-21 08:39:47 -0500

    CON-245: Fix corner cases with taking intersection of sorted sets

--- concourse/src/test/java/org/cinchapi/concourse/util/TSetsTest.java
+++ concourse/src/test/java/org/cinchapi/concourse/util/TSetsTest.java
@@ -23,6 +23,7 @@
  */
 package org.cinchapi.concourse.util;
 
+import java.util.Arrays;
 import java.util.Set;
 
 import org.cinchapi.concourse.time.Time;
@@ -47,9 +48,35 @@ public class TSetsTest {
         Assert.assertEquals(TSets.intersection(a, b), Sets.intersection(a, b)
                 .copyInto(Sets.<Integer> newHashSet()));
     }
-    
+
     @Test
-    public void testIntersection(){
+    public void testSortedIntersectionReproCON_245() {
+        Set<Long> a = Sets.newTreeSet(Arrays.<Long> asList(1424523725311000L,
+                1424523725341000L, 1424523725370000L, 1424523725421002L,
+                1424523725491000L, 1424523725514000L, 1424523725534000L,
+                1424523725565000L, 1424523725584003L, 1424523725605000L,
+                1424523725622002L, 1424523725641002L, 1424523725659002L,
+                1424523725676004L, 1424523725691000L, 1424523725710000L,
+                1424523725724000L, 1424523725738000L, 1424523725786002L,
+                1424523725801000L, 1424523725823002L, 1424523725841000L,
+                1424523725868000L, 1424523725895000L, 1424523725909000L,
+                1424523725924000L, 1424523725937002L, 1424523725963000L,
+                1424523725975002L, 1424523725987000L, 1424523725999003L,
+                1424523726012000L, 1424523726038000L, 1424523726050000L));
+        Set<Long> b = Sets.newTreeSet(Arrays.asList(1424523725341000L,
+                1424523725491000L, 1424523725514000L, 1424523725534000L,
+                1424523725584003L, 1424523725605000L, 1424523725641002L,
+                1424523725659002L, 1424523725691000L, 1424523725724000L,
+                1424523725753000L, 1424523725786002L, 1424523725801000L,
+                1424523725823002L, 1424523725841000L, 1424523725937002L,
+                1424523725963000L, 1424523725975002L, 1424523725987000L,
+                1424523725999003L, 1424523726012000L, 1424523726050000L));
+        Assert.assertEquals(TSets.intersection(a, b), Sets.intersection(a, b)
+                .copyInto(Sets.<Long> newHashSet()));
+    }
+
+    @Test
+    public void testIntersection() {
         Set<Integer> a = Sets.newHashSet();
         Set<Integer> b = Sets.newHashSet();
         populate(a);
@@ -57,9 +84,9 @@ public class TSetsTest {
         Assert.assertEquals(TSets.intersection(a, b), Sets.intersection(a, b)
                 .copyInto(Sets.<Integer> newHashSet()));
     }
-    
+
     @Test
-    public void testMixedIntersection(){
+    public void testMixedIntersection() {
         Set<Integer> a = Sets.newTreeSet();
         Set<Integer> b = Sets.newHashSet();
         populate(a);
@@ -67,15 +94,15 @@ public class TSetsTest {
         Assert.assertEquals(TSets.intersection(a, b), Sets.intersection(a, b)
                 .copyInto(Sets.<Integer> newHashSet()));
     }
-    
+
     @Test
-    public void testUnion(){
+    public void testUnion() {
         Set<Integer> a = Sets.newHashSet();
         Set<Integer> b = Sets.newHashSet();
         populate(a);
         populate(b);
-        Assert.assertEquals(TSets.union(a, b), Sets.union(a, b)
-                .copyInto(Sets.<Integer> newHashSet()));
+        Assert.assertEquals(TSets.union(a, b),
+                Sets.union(a, b).copyInto(Sets.<Integer> newHashSet()));
     }
 
     /**
