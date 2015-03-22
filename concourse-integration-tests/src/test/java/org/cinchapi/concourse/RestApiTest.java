781eab14335861542f246183f94e9b47229ec585
Jeff Nelson <jtnelson1@gmail.com>
2015-03-22 07:31:00 -0400

    add functionality to preserve cookies and util methods to get body responses in a more streamlined manner

--- concourse-integration-tests/src/test/java/org/cinchapi/concourse/RestApiTest.java
+++ concourse-integration-tests/src/test/java/org/cinchapi/concourse/RestApiTest.java
@@ -16,6 +16,10 @@
 package org.cinchapi.concourse;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.cinchapi.concourse.server.ConcourseServer;
@@ -29,8 +33,11 @@ import org.junit.Test;
 
 import com.google.common.base.Strings;
 import com.google.common.base.Throwables;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
@@ -45,14 +52,49 @@ import com.squareup.okhttp.Response;
 public class RestApiTest extends ConcourseIntegrationTest {
 
     /**
+     * Return the response body as the appropriate Java object.
+     * 
+     * @param response
+     * @return the response body
+     */
+    private static <T> T bodyAsJava(Response response, TypeToken<T> type) {
+        Type type0 = type.getType();
+        return new Gson().fromJson(bodyAsJson(response), type0);
+    }
+    
+    /**
+     * Return a JsonElement representation of the response body.
+     * 
+     * @param response
+     * @param the json response
+     */
+    private static JsonElement bodyAsJson(Response response) {
+        try {
+            String body = response.body().string();
+            JsonElement json = new JsonParser().parse(body);
+            return json;
+        }
+        catch (IOException e) {
+            throw Throwables.propagate(e);
+        }
+    }
+    
+    /**
      * JSON media type
      */
     private static final MediaType JSON = MediaType
             .parse("application/json; charset=utf-8");
     private String base = "http://localhost:";
+
     private OkHttpClient http = new OkHttpClient();
 
     private HttpServer httpServer;
+
+    {
+        CookieManager cm = new CookieManager();
+        cm.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
+        http.setCookieHandler(cm);
+    }
 
     @Override
     public void afterEachTest() {
@@ -83,19 +125,31 @@ public class RestApiTest extends ConcourseIntegrationTest {
     }
 
     @Test
+    public void testGetRoot(){
+        login();
+        List<Long> r1 = bodyAsJava(get("/"), new TypeToken<List<Long>>(){});
+        Assert.assertTrue(r1.isEmpty());
+        long record = client.add("foo", "bar");
+        List<Long> r2 = bodyAsJava(get("/"), new TypeToken<List<Long>>(){});
+        Assert.assertTrue(r2.contains(record));
+    }
+
+    @Test
     public void testLogin() throws IOException {
         Response resp = login();
         Assert.assertEquals(200, resp.code());
-        JsonObject body = (JsonObject) new JsonParser().parse(resp.body().string());
+        JsonObject body = (JsonObject) new JsonParser().parse(resp.body()
+                .string());
         Assert.assertEquals("default", body.get("environment").getAsString());
     }
-    
+
     @Test
     public void testLoginNonDefaultEnvironment() throws IOException {
         String environment = TestData.getStringNoDigits().replaceAll(" ", "");
         Response resp = login(environment);
         Assert.assertEquals(200, resp.code());
-        JsonObject body = (JsonObject) new JsonParser().parse(resp.body().string());
+        JsonObject body = (JsonObject) new JsonParser().parse(resp.body()
+                .string());
         Assert.assertEquals(environment, body.get("environment").getAsString());
     }
 
@@ -137,7 +191,8 @@ public class RestApiTest extends ConcourseIntegrationTest {
         JsonObject creds = new JsonObject();
         creds.addProperty("username", "admin");
         creds.addProperty("password", "admin");
-        return post(environment + "/login", creds.toString());
+        Response resp = post(environment + "/login", creds.toString());
+        return resp;
     }
 
     /**
