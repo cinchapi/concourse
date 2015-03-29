480311c1c6d8fdea52afcf801fb31e6d7e4ee541
Jeff Nelson <jtnelson1@gmail.com>
2015-03-29 09:50:11 -0400

    add util for getting simple strings

--- concourse-driver-java/src/main/java/org/cinchapi/concourse/util/Random.java
+++ concourse-driver-java/src/main/java/org/cinchapi/concourse/util/Random.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2013-2015 Cinchapi, Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -191,7 +191,7 @@ public abstract class Random {
     }
 
     /**
-     * Return a get string, possibly with digits.
+     * Return a string, possibly with digits.
      * 
      * @return the string
      */
@@ -199,6 +199,19 @@ public abstract class Random {
         String string = null;
         while (Strings.nullToEmpty(string).trim().isEmpty()) {
             string = strand.nextStringAllowDigits();
+        }
+        return string;
+    }
+
+    /**
+     * Return a simple string with no digits or spaces.
+     * 
+     * @return the string
+     */
+    public static String getSimpleString() {
+        String string = null;
+        while (Strings.nullToEmpty(string).trim().isEmpty()) {
+            string = getStringNoDigits().replaceAll(" ", "");
         }
         return string;
     }
