94fd6faaf20fb9c6f0571e1cbbc71dafa28985d8
Jeff Nelson <jtnelson1@gmail.com>
2015-04-02 08:40:21 -0400

    optimize transporting within TransactionQueue

--- concourse-server/src/main/java/org/cinchapi/concourse/server/storage/temp/TransactionQueue.java
+++ concourse-server/src/main/java/org/cinchapi/concourse/server/storage/temp/TransactionQueue.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2013-2015 Cinchapi, Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,6 +15,7 @@
  */
 package org.cinchapi.concourse.server.storage.temp;
 
+import org.cinchapi.concourse.server.storage.PermanentStore;
 import org.cinchapi.concourse.server.storage.cache.BloomFilter;
 
 /**
@@ -24,6 +25,12 @@ import org.cinchapi.concourse.server.storage.cache.BloomFilter;
  * @author Jeff Nelson
  */
 public class TransactionQueue extends Queue {
+
+    /**
+     * An empty array of writes that is used to specify type conversion in the
+     * {@link ArrayList#toArray(Object[])} method.
+     */
+    private static final Write[] EMPTY_WRITES_ARRAY = new Write[0];
 
     /**
      * The bloom filter used to speed up verifies.
@@ -47,6 +54,21 @@ public class TransactionQueue extends Queue {
         }
         else {
             return false;
+        }
+    }
+
+    @Override
+    public void transport(PermanentStore destination, boolean sync) {
+        // For transactions, this method will only be called once, so we can
+        // optimize it by not using the services of an Iterator (e.g. hasNext(),
+        // remove(), etc) and, if the number of writes in the Queue is large
+        // enough, grabbing elements from the backing array directly.
+        int length = writes.size();
+        Write[] elts = length > 10000 ? writes.toArray(EMPTY_WRITES_ARRAY)
+                : null;
+        for (int i = 0; i < length; ++i) {
+            Write write = elts == null ? writes.get(i) : elts[i];
+            destination.accept(write, sync);
         }
     }
 
