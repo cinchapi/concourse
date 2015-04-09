4b7751268768fd32487719f8a024529252f336e0
Jeff Nelson <jtnelson1@gmail.com>
2015-04-09 07:29:36 -0400

    doc update

--- concourse-driver-java/src/main/java/org/cinchapi/concourse/time/AtomicClock.java
+++ concourse-driver-java/src/main/java/org/cinchapi/concourse/time/AtomicClock.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2013-2015 Cinchapi, Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,7 +38,7 @@ public class AtomicClock {
     // http://stackoverflow.com/questions/9191288/creating-a-unique-timestamp-in-java
     public long time() {
         long now = System.currentTimeMillis() * 1000;
-        while (true) {
+        for (;;) {
             long lastTime = time.get();
             if(lastTime >= now) {
                 now = lastTime + 1;
