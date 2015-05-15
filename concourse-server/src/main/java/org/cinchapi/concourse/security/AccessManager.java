e636a5603f8c59c31b2aa674536322415de2ac0e
Jeff Nelson <jtnelson1@gmail.com>
2015-05-15 08:31:14 -0400

    clean up access manager code

--- concourse-server/src/main/java/org/cinchapi/concourse/security/AccessManager.java
+++ concourse-server/src/main/java/org/cinchapi/concourse/security/AccessManager.java
@@ -17,6 +17,7 @@ package org.cinchapi.concourse.security;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
 import java.security.SecureRandom;
 import java.util.Collections;
@@ -35,12 +36,12 @@ import org.cinchapi.concourse.server.io.Serializables;
 import org.cinchapi.concourse.thrift.AccessToken;
 import org.cinchapi.concourse.time.Time;
 import org.cinchapi.concourse.util.ByteBuffers;
-import org.cinchapi.concourse.util.TStrings;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.DateTimeFormatterBuilder;
 
 import static com.google.common.base.Preconditions.*;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 import com.google.common.base.Throwables;
@@ -94,74 +95,120 @@ public class AccessManager {
      * @param username
      * @return {@code true} if {@code username} is valid format
      */
-    protected static boolean isAcceptableUsername(ByteBuffer username) { // visible
-                                                                         // for
-                                                                         // testing
-        String usernameStr = new String(username.array());
-        return !Strings.isNullOrEmpty(usernameStr)
-                && !usernameStr
-                        .contains(TStrings.REGEX_GROUP_OF_ONE_OR_MORE_WHITESPACE_CHARS);
+    @VisibleForTesting
+    protected static boolean isAcceptableUsername(ByteBuffer username) {
+        username.mark();
+        CharBuffer chars = username.asCharBuffer();
+        boolean acceptable = chars.capacity() > 0;
+        while (acceptable && chars.hasRemaining()) {
+            char c = chars.get();
+            if(Character.isWhitespace(c)) {
+                acceptable = false;
+                break;
+            }
+        }
+        username.reset();
+        return acceptable;
     }
 
     /**
-     * Return {@code true} if {@code username} is reserved.
-     * 
-     * @param username
-     * @return {@code true} if ({@code username} is reserved
-     */
-    protected static boolean isReservedUsername(ByteBuffer username) { // visible
-                                                                       // for
-                                                                       // testing
-        String usernameStr = new String(username.array());
-        return usernameStr.equalsIgnoreCase(DELETED_USERNAME);
-    }
-
-    /**
-     * Return {@code true} if {@code password} is in valid format,
-     * in which it must not be null or empty, or contain fewer than
-     * 3 characters.
+     * Return {@code true} if {@code password} is in valid format, which means
+     * it meets the following requirements:
+     * <ul>
+     * <li>{@value #MIN_PASSWORD_LENGTH} or more characters</li>
+     * <li>At least one non whitespace character</li>
+     * </ul>
      * 
      * @param password
      * @return {@code true} if {@code password} is valid format
      */
-    protected static boolean isSecurePassword(ByteBuffer password) { // visible
-                                                                     // for
-                                                                     // testing
-        String passwordStr = new String(password.array());
-        return !Strings.isNullOrEmpty(passwordStr) && passwordStr.length() >= 3;
+    @VisibleForTesting
+    protected static boolean isSecurePassword(ByteBuffer password) {
+        password.mark();
+        boolean secure = false;
+        CharBuffer chars = password.asCharBuffer();
+        if(password.capacity() >= MIN_PASSWORD_LENGTH) {
+            while (chars.hasRemaining()) {
+                char c = chars.get();
+                if(!Character.isWhitespace(c)) {
+                    secure = true;
+                    break;
+                }
+            }
+        }
+        password.reset();
+        return secure;
     }
 
-    // The AccessManager keeps track of user sessions and their associated
-    // AccessTokens. An AccessToken is valid for a limited amount of time.
+    /**
+     * The number of hours for which an AccessToken is valid.
+     */
     private static final int ACCESS_TOKEN_TTL = 24;
+
+    /**
+     * The unit of time for which an AccessToken is valid.
+     */
     private static final TimeUnit ACCESS_TOKEN_TTL_UNIT = TimeUnit.HOURS;
-    private static final String ADMIN_PASSWORD = ByteBuffers
+
+    /**
+     * The default admin password. If the AccessManager does not have any users,
+     * it will automatically create an admin with this password.
+     */
+    private static final String DEFAULT_ADMIN_PASSWORD = ByteBuffers
             .encodeAsHex(ByteBuffer.wrap("admin".getBytes()));
 
-    // The AccessManager stores credentials in memory for fast processing and
-    // backs them up on disk for persistence.
-    private static final String ADMIN_USERNAME = ByteBuffers
+    /**
+     * The default admin username. If the AccessManager does not have any users,
+     * it will automatically create an admin with this username.
+     */
+    private static final String DEFAULT_ADMIN_USERNAME = ByteBuffers
             .encodeAsHex(ByteBuffer.wrap("admin".getBytes()));
-    // Reserved username that is used to display deleted user
-    // in history output.
-    private static final String DELETED_USERNAME = "Unknown";
-    private static final String PASSWORD_KEY = "password"; // table value as hex
-    private static final String SALT_KEY = "salt"; // table value as hex
 
-    // Column keys in table of credentials.
-    private static final String USERNAME_KEY = "username"; // table value as hex
+    /**
+     * The minimum number of character that must be contained in a password.
+     */
+    private static final int MIN_PASSWORD_LENGTH = 3;
 
+    /**
+     * The column that contains a user's password in the {@link #credentials}
+     * table.
+     */
+    private static final String PASSWORD_KEY = "password";
+
+    /**
+     * The column that contains a user's salt in the {@link #credentials} table.
+     */
+    private static final String SALT_KEY = "salt";
+
+    /**
+     * The column that contains a user's username in the {@link #credentials}
+     * table.
+     */
+    private static final String USERNAME_KEY = "username";
+
+    /**
+     * The store where the credentials are serialized on disk.
+     */
     private final String backingStore;
-    // The new uid is generated by incrementing the largest existing uid
-    // in the credentials mapping in memory.
+
+    /**
+     * A counter that assigns user ids.
+     */
     private AtomicInteger counter;
 
+    /**
+     * A table in memory that holds the user credentials.
+     */
     private final HashBasedTable<Short, String, Object> credentials;
 
-    // Concurrency controls.
+    /**
+     * Concurrency control.
+     */
     private final StampedLock lock = new StampedLock();
 
-    // Deals with all the access tokens for user sessions
+    /**
+     * Handles access tokens.
+     */
     private final AccessTokenManager tokenManager;
 
     /**
@@ -188,8 +235,8 @@ public class AccessManager {
             credentials = HashBasedTable.create();
             // If there are no credentials (which implies this is a new server)
             // add the default admin username/password
-            createUser(ByteBuffers.decodeFromHex(ADMIN_USERNAME),
-                    ByteBuffers.decodeFromHex(ADMIN_PASSWORD));
+            createUser(ByteBuffers.decodeFromHex(DEFAULT_ADMIN_USERNAME),
+                    ByteBuffers.decodeFromHex(DEFAULT_ADMIN_PASSWORD));
         }
     }
 
@@ -210,8 +257,6 @@ public class AccessManager {
     public void createUser(ByteBuffer username, ByteBuffer password) {
         Preconditions.checkArgument(isAcceptableUsername(username),
                 "Username must not be empty, or contain any whitespace.");
-        Preconditions.checkArgument(!isReservedUsername(username),
-                "This username is reserved.");
         Preconditions.checkArgument(isSecurePassword(password),
                 "Password must not be empty, or have fewer than 3 characters.");
         long stamp = lock.writeLock();
@@ -236,7 +281,7 @@ public class AccessManager {
         long stamp = lock.writeLock();
         try {
             String hex = ByteBuffers.encodeAsHex(username);
-            checkArgument(!hex.equals(ADMIN_USERNAME),
+            checkArgument(!hex.equals(DEFAULT_ADMIN_USERNAME),
                     "Cannot revoke access for the admin user!");
             short uid = getUidByUsername0(username);
             credentials.remove(uid, USERNAME_KEY);
