0985e73062d66ca89216014f0e4cf867f2c4a238
Jeff Nelson <jtnelson1@gmail.com>
2015-05-17 13:37:50 -0400

    add ClientSecurity methods to encrypt/decrypt with a provided key

--- concourse-driver-java/src/main/java/org/cinchapi/concourse/security/ClientSecurity.java
+++ concourse-driver-java/src/main/java/org/cinchapi/concourse/security/ClientSecurity.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2013-2015 Cinchapi, Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,6 +21,8 @@ import java.security.SecureRandom;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
+import org.cinchapi.concourse.util.ByteBuffers;
+
 import com.google.common.base.Throwables;
 
 /**
@@ -32,31 +34,16 @@ import com.google.common.base.Throwables;
 public class ClientSecurity {
 
     /**
-     * The client specific key that is used to encrypt/decrypt data. This key is
-     * valid for the duration of the client JVM's lifetime.
-     */
-    private static final byte[] KEY = new byte[16];
-    static {
-        new SecureRandom().nextBytes(KEY);
-    }
-
-    /**
-     * Encrypt the specified {@code data} string.
+     * Decrpyt the specified {@code data} buffer. This method returns a byte
+     * buffer instead of a string so that the caller can set the buffer to
+     * {@code null} once it is not longer in useful scope. Doing so will help to
+     * narrow the window in which vulnerable information resides in memory.
      * 
      * @param data
-     * @return a byte buffer with the encrypted data
+     * @return a byte buffer with the encrypted data.
      */
-    public static ByteBuffer encrypt(String data) {
-        try {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            SecretKeySpec secretKey = new SecretKeySpec(KEY, "AES");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            return ByteBuffer.wrap(cipher.doFinal(data.getBytes()));
-        }
-        catch (Exception e) {
-            throw Throwables.propagate(e);
-        }
-
+    public static ByteBuffer decrypt(ByteBuffer data) {
+        return decrypt(data, DEFAULT_KEY);
     }
 
     /**
@@ -68,16 +55,91 @@ public class ClientSecurity {
      * @param data
      * @return a byte buffer with the encrypted data.
      */
-    public static ByteBuffer decrypt(ByteBuffer data) {
+    public static ByteBuffer decrypt(ByteBuffer data, byte[] key) {
         try {
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            SecretKeySpec secretKey = new SecretKeySpec(KEY, "AES");
+            SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
             cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            return ByteBuffer.wrap(cipher.doFinal(data.array()));
+            return ByteBuffer
+                    .wrap(cipher.doFinal(ByteBuffers.toByteArray(data)));
         }
         catch (Exception e) {
             throw Throwables.propagate(e);
         }
     }
+
+    /**
+     * Encrypt the {@code data} using the {@code key}
+     * 
+     * @param data
+     * @param key
+     * @return the encrypted payload
+     */
+    public static ByteBuffer encrypt(byte[] data, byte[] key) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return ByteBuffer.wrap(cipher.doFinal(data));
+        }
+        catch (Exception e) {
+            throw Throwables.propagate(e);
+        }
+    }
+
+    /**
+     * Encrypt the {@code data} using the {@code key}
+     * 
+     * @param data
+     * @param key
+     * @return the encrypted payload
+     */
+    public static ByteBuffer encrypt(ByteBuffer data, byte[] key) {
+        return encrypt(ByteBuffers.toByteArray(data), key);
+    }
+
+    /**
+     * Encrypt the specified {@code data} string.
+     * 
+     * @param data
+     * @return a byte buffer with the encrypted data
+     */
+    public static ByteBuffer encrypt(String data) {
+        return encrypt(data, DEFAULT_KEY);
+    }
+
+    /**
+     * Encrypt the {@code data} string using the {@code key}
+     * 
+     * @param data
+     * @param key
+     * @return the encrypted payload
+     */
+    public static ByteBuffer encrypt(String data, byte[] key) {
+        return encrypt(data.getBytes(), key);
+    }
+
+    /**
+     * The source of secure randomness.
+     */
+    private static final SecureRandom srand = new SecureRandom();
+
+    /**
+     * Generate a random secret key that can be used for encryption and
+     * decryption.
+     * 
+     * @return the key
+     */
+    public static byte[] generateSecretKey() {
+        byte[] key = new byte[16];
+        srand.nextBytes(key);
+        return key;
+    }
+
+    /**
+     * The client specific key that is used to encrypt/decrypt data. This key is
+     * valid for the duration of the client JVM's lifetime.
+     */
+    private static final byte[] DEFAULT_KEY = generateSecretKey();
 
 }
