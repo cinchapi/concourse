1a4b0091187fcde792ca7c89b18e66c69fb56969
Jeff Nelson <jtnelson1@gmail.com>
2015-06-30 08:28:39 -0400

    doc update

--- mockcourse/src/main/groovy/org/cinchapi/mockcourse/Mockcourse.groovy
+++ mockcourse/src/main/groovy/org/cinchapi/mockcourse/Mockcourse.groovy
@@ -41,12 +41,9 @@ import groovy.json.JsonOutput;
  * MOCKOURSE IS NOT SUITABLE FOR PRODUCTION!!!
  *
  * Mockcourse is designed to be used as in unit tests for client drivers. As
- * such
- * this implementation is likely incomplete and somewhat buggy. When unit
- * testing
- * client drivers, we are not looking to validate server side functionality,
- * instead we are only looking to show that the driver is able to correctly send
- * data over the wire and recieve the same data back.
+ * such this implementation is likely incomplete and somewhat buggy. When unit
+ * testing client drivers, we are not looking to validate server side
+ * functionality, instead we are only looking to show that the driver is able to * correctly send data over the wire and recieve the same data back.
  *
  * @author jnelson
  */
