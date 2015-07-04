5a3772e602389598d645f80d2be082656b1b8a96
Jeff Nelson <jtnelson1@gmail.com>
2015-07-03 20:56:45 -0400

    cash: comment logic to handle missing arg commas

--- concourse-shell/src/main/java/org/cinchapi/concourse/shell/ConcourseShell.java
+++ concourse-shell/src/main/java/org/cinchapi/concourse/shell/ConcourseShell.java
@@ -482,7 +482,7 @@ public final class ConcourseShell {
      */
     public String evaluate(String input) throws IrregularEvaluationResult {
         input = SyntaxTools.handleShortSyntax(input, methods);
-        input = SyntaxTools.handleMissingArgCommas(input);
+//        input = SyntaxTools.handleMissingArgCommas(input);
         String inputLowerCase = input.toLowerCase();
 
         // NOTE: These must always be set before evaluating a line just in case
