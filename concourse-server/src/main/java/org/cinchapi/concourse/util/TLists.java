ce146d7f43d7d754224a5917b9d6d443d3a9da23
Jeff Nelson <jtnelson1@gmail.com>
2015-07-12 07:04:08 -0500

    add a method to convert a list to an array using casting

--- concourse-server/src/main/java/org/cinchapi/concourse/util/TLists.java
+++ concourse-server/src/main/java/org/cinchapi/concourse/util/TLists.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2013-2015 Cinchapi, Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -42,6 +42,19 @@ public final class TLists {
         for (int i = 1; i < lists.length; ++i) {
             lists[i].retainAll(intersection);
         }
+    }
+
+    /**
+     * Syntactic sugar for transforming a generic {@link List} into an array
+     * with the same generic type. This method performs the casting so that the
+     * calling code doesn't have to.
+     * 
+     * @param list
+     * @return the array
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T[] toArrayCasted(List<T> list) {
+        return (T[]) list.toArray();
     }
 
     private TLists() {/* noop */}
