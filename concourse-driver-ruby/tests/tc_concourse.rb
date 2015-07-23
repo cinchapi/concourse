dfcc37d4ec64f94b6ee84f585a448ff43214924d
Jeff Nelson <jtnelson1@gmail.com>
2015-07-23 11:01:26 -0500

    ruby-driver: create base integration test

--- concourse-driver-ruby/tests/tc_concourse.rb
+++ concourse-driver-ruby/tests/tc_concourse.rb
@@ -2,50 +2,9 @@ require 'test/unit'
 require 'socket'
 require_relative '../lib/concourse'
 require_relative 'testutils'
+require_relative 'tc_base'
 
-class RubyClientDriverTest < Test::Unit::TestCase
-
-    @@client = nil
-    @@has_setup = false
-
-    def initialize(arg)
-        super(arg)
-        if(!@@has_setup)
-            path = File.expand_path('../../../mockcourse/mockcourse', __FILE__)
-            port = get_open_port
-            path = "#{path} #{port} > /dev/null 2>&1 &"
-            @@pid = Process.spawn(path, :pgroup => true)
-            tries = 5
-            while tries > 0 and @@client.nil?
-                tries -= 1
-                sleep(1) # wait for Mockcourse to start
-                begin
-                    @@client = Concourse.new(port:port)
-                rescue Exception => ex
-                    if tries == 0
-                        raise ex
-                    else
-                        next
-                    end
-                end
-            end
-            @@pid = get_mockcourse_pid
-            ObjectSpace.define_finalizer(self, self.class.finalize(@@pid))
-            @@has_setup = true
-        end
-    end
-
-    def self.finalize(pid)
-        proc { exec("kill -9 #{pid}") }
-    end
-
-    def setup
-        @client = @@client
-    end
-
-    def teardown
-        @client.logout
-    end
+class RubyClientDriverTest < IntegrationBaseTest
 
     def test_abort
         @client.stage
@@ -74,32 +33,5 @@ class RubyClientDriverTest < Test::Unit::TestCase
         stored = @client.get key, record
         assert_equal(value, stored)
     end
-
-    def get_open_port
-        socket = Socket.new(:INET, :STREAM, 0)
-        socket.bind(Addrinfo.tcp("127.0.0.1", 0))
-        port =  socket.local_address.ip_port
-        socket.close
-        port
-    end
-
-    def get_mockcourse_pid
-        script = File.expand_path('../../../mockcourse/getpid', __FILE__)
-        out = `#{script}`
-        pids = out.lines.map(&:chomp)
-        pid = 0
-        _delta = 0
-        for x in pids
-            delta = (x.to_i - @@pid).abs
-            if pid == 0 or (pid != 0 and delta < _delta)
-                _delta = delta
-                pid = x
-            end
-        end
-        pid
-    end
-
-    private :get_open_port, :get_mockcourse_pid
-
 
 end
