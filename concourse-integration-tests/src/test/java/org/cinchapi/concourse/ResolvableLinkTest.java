1d4167f7e9fe334f6504d8d3314136c2a650de15
Jeff Nelson <jtnelson1@gmail.com>
2015-08-22 13:54:52 -0400

    add more unit tests

--- concourse-integration-tests/src/test/java/org/cinchapi/concourse/ResolvableLinkTest.java
+++ concourse-integration-tests/src/test/java/org/cinchapi/concourse/ResolvableLinkTest.java
@@ -23,6 +23,7 @@ import org.cinchapi.concourse.util.TestData;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.google.common.collect.Lists;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
@@ -80,6 +81,59 @@ public class ResolvableLinkTest extends ConcourseIntegrationTest {
         Set<Long> records = client.find("name = John");
         for(long record : records){
             Assert.assertTrue(client.find("similar_to lnk2 "+record).isEmpty());
+        }
+    }
+    
+    @Test
+    public void testInsertLocalAndRemoteResolvableLink(){
+        client.add("foo", 1, 1);
+        client.add("name", "Jeff", 1);
+        client.add("foo", 2, 2);
+        client.add("name", "Jeffery", 2);
+        JsonObject json1 = new JsonObject();
+        JsonObject json2 = new JsonObject();
+        JsonObject json3 = new JsonObject();
+        JsonObject json4 = new JsonObject();
+        json1.addProperty("foo", 10);
+        json1.addProperty("name", "Jeff");
+        json1.addProperty("similar_to", "@foo < 50 and name = Jeff@");
+        json2.addProperty("foo", 20);
+        json2.addProperty("name", "Jeff");
+        json2.addProperty("similar_to", "@foo < 50 and name = Jeff@");
+        json3.addProperty("foo", 30);
+        json3.addProperty("name", "John");
+        json3.addProperty("similar_to", "@foo < 50 and name = Jeff@");
+        json4.addProperty("foo", 40);
+        json4.addProperty("name", "Jeff");
+        json4.addProperty("similar_to", "@foo < 50 and name = Jeff@");
+        JsonArray array = new JsonArray();
+        array.add(json1);
+        array.add(json2);
+        array.add(json3);
+        array.add(json4);
+        String json = array.toString();
+        client.insert(json);
+        Map<Long, Set<Object>> data = client.select("similar_to", "name = Jeff and foo != 1");
+        for(Set<Object> set : data.values()){
+            Assert.assertEquals(3, set.size());
+        }
+        Set<Long> records = client.find("name != Jeff");
+        for(long record : records){
+            Assert.assertTrue(client.find("similar_to lnk2 "+record).isEmpty());
+        }
+    }
+    
+    @Test
+    public void testInsertRemoteResolvableLinkInExistingRecord(){
+        long record1 = client.add("company", "Cinchapi");
+        long record2 = client.add("company", "Cinchapi");
+        long record3 = client.add("company", "Cinchapi");
+        JsonObject json = new JsonObject();
+        json.addProperty("co_workers", Convert.stringToResolvableLinkInstruction("company = Cinchapi"));
+        client.insert(json.toString(), Lists.newArrayList(record1, record2, record3));
+        Map<Long, Set<Object>> data = client.select("co_workers", Lists.newArrayList(record1, record2, record3));
+        for(Set<Object> set : data.values()){
+            Assert.assertEquals(2, set.size());
         }
     }
 
