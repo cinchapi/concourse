699fcc950911abcee55d51e7134898753bb8ff1f
Jeff Nelson <jtnelson1@gmail.com>
2015-08-27 07:15:06 -0400

    move TLists class from concourse-server to concourse-driver-java

--- concourse-driver-java/src/test/java/org/cinchapi/concourse/util/TListsTest.java
+++ concourse-driver-java/src/test/java/org/cinchapi/concourse/util/TListsTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2013-2015 Cinchapi Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,8 +17,6 @@ package org.cinchapi.concourse.util;
 
 import java.util.List;
 
-import org.cinchapi.concourse.ConcourseBaseTest;
-import org.cinchapi.concourse.testing.Variables;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -29,24 +27,22 @@ import com.google.common.collect.Lists;
  * 
  * @author Jeff Nelson
  */
-public class TListsTest extends ConcourseBaseTest {
+public class TListsTest {
 
     @SuppressWarnings("unchecked")
     @Test
     public void testRetainIntersection() {
-        List<Integer> common = Variables.register("common",
-                Lists.<Integer> newArrayList());
-        List<Integer>[] lists = new List[TestData.getScaleCount()];
-        int max = TestData.getScaleCount();
+        List<Integer> common = Lists.newArrayList();
+        List<Integer>[] lists = new List[Random.getScaleCount()];
+        int max = Random.getScaleCount();
         for (int i = 0; i < max; i++) {
             common.add(i);
         }
         for (int i = 0; i < lists.length; i++) {
-            lists[i] = Variables.register("list_" + i,
-                    Lists.<Integer> newArrayList());
+            lists[i] = Lists.<Integer> newArrayList();
             lists[i].addAll(common);
-            for (int j = 0; j < TestData.getScaleCount(); j++) {
-                lists[i].add(TestData.getScaleCount() + max);
+            for (int j = 0; j < Random.getScaleCount(); j++) {
+                lists[i].add(Random.getScaleCount() + max);
             }
             lists[i] = Lists.newArrayList(lists[i]);
         }
