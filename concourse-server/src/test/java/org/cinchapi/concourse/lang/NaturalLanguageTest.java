6e042b9fe49a85efc5fb416e20dc3d854d967992
Jeff Nelson <jtnelson1@gmail.com>
2015-08-27 16:51:14 -0400

    comparse micros instead of strings because of timezone differences on the server

--- concourse-server/src/test/java/org/cinchapi/concourse/lang/NaturalLanguageTest.java
+++ concourse-server/src/test/java/org/cinchapi/concourse/lang/NaturalLanguageTest.java
@@ -30,8 +30,8 @@ public class NaturalLanguageTest extends ConcourseBaseTest {
     @Test
     public void testParseTimeStringInDefaultFormat() {
         String str = "Sat Mar 07, 2015 @ 8:11:35:36 PM EST";
-        Timestamp ts = Timestamp.fromMicros(NaturalLanguage.parseMicros(str));
-        Assert.assertEquals(str, ts.toString());
+        long expected = Timestamp.parse(str, Timestamp.DEFAULT_FORMATTER).getMicros();
+        Assert.assertEquals(expected, NaturalLanguage.parseMicros(str));
     }
 
 }
