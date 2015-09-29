f8fa189452c057fd344d7d1fe655f5f567441b90
Jeff Nelson <jeff@cinchapi.com>
2015-09-28 20:22:29 -0400

    don 't try to run bats test on README.md

--- utils/run-bash-tests.sh
+++ utils/run-bash-tests.sh
@@ -3,4 +3,4 @@
 # Ensure that this script operates from the directory in which it resides
 cd "${0%/*}"
 
-./bats/bin/bats ../concourse-bash-tests/*
+./bats/bin/bats ../concourse-bash-tests/*[!README.md]
