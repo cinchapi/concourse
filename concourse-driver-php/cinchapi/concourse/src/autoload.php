6bb0ef63618d74b48262b78028cd99641743446f
Jeff Nelson <jeff@cinchapi.com>
2015-10-10 16:04:58 -0400

    php-driver: add require_directory function

--- concourse-driver-php/cinchapi/concourse/src/autoload.php
+++ concourse-driver-php/cinchapi/concourse/src/autoload.php
@@ -1,6 +1,5 @@
 <?php
-
-/* 
+/*
  * Copyright 2015 Cinchapi Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,17 +15,39 @@
  * limitations under the License.
  */
 
-/*
- * Central place to require everything that needs to be autoloaded throughout
- * the project.
+/**
+ * This file is the central place to require everything that needs to be
+ * autoloaded throughout the project.
  */
-
 require_once dirname(__FILE__) . "/../../../vendor/autoload.php";
-require_once dirname(__FILE__) . "/thrift/ConcourseService.php";
-require_once dirname(__FILE__) . "/thrift/shared/Types.php";
-require_once dirname(__FILE__) . "/thrift/data/Types.php";
-require_once dirname(__FILE__) . "/Concourse.php";
-require_once dirname(__FILE__) . "/Convert.php";
-require_once dirname(__FILE__) . "/Tag.php";
-require_once dirname(__FILE__) . "/Link.php";
-require_once dirname(__FILE__) . "/utils.php";
+require_directory(dirname(__FILE__));
+
+/**
+* @ignore
+* Return {@code true} if the {@code $haystack} ends with the {@code $needle}.
+* @param string $haystack
+* @param string $needle
+* @return boolean
+*/
+function str_ends_with($haystack, $needle){
+    return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
+}
+
+/**
+ * @ignore
+ * Require all of the files in the specified {@code $directory} and recursively * do so if {@code $recursive} is true.
+ * @param string $directory - the directory that contains the files to require
+ * @param string $recursive - a flag that controls whether this function will
+ * descend into subdirectories to require those files (default: true).
+ */
+function require_directory($directory, $recursive=true){
+    foreach(scandir($directory) as $filename){
+        $path = $directory . DIRECTORY_SEPARATOR . $filename;
+        if($recursive && is_dir($path) && !str_ends_with($path, ".")){
+            require_directory($path, $recursive);
+        }
+        else if (str_ends_with($path, ".php")){
+            require_once $path;
+        }
+    }
+}
