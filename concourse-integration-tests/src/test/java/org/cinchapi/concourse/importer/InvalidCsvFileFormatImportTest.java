6953f85029746684172d110b5d2a828ae5a46c88
Henry Cuffy <cuffyhenry@MacBook-Pro-2.Speedport_W723_V_Typ_A_1_01_010>
2015-10-12 20:04:35 +0200

    csv test class and sample file

---
+++ concourse-integration-tests/src/test/java/org/cinchapi/concourse/importer/InvalidCsvFileFormatImportTest.java
@@ -0,0 +1,21 @@
+package org.cinchapi.concourse.importer;
+
+import org.cinchapi.concourse.util.Resources;
+import org.junit.Test;
+
+public class InvalidCsvFileFormatImportTest extends CsvImportTest{
+
+	@Override
+	protected String getImportPath() {
+		return "invalidLine.csv";
+	}
+	
+	@Override
+	@Test (expected = IllegalArgumentException.class)
+	public void testImport() {
+        String file = Resources.get("/" + getImportPath()).getFile();
+        importer.importFile(file);
+	}
+	
+	
+}
