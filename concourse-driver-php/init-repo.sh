ffbfd6bd1ca6ce92a4e38ea8b0b9102e5b47c929
Jeff Nelson <jeff@cinchapi.com>
2015-10-24 08:50:22 -0400

    [auto-build] packagist upload for concourse-php-driver-0.5.0.172-SNAPSHOT

--- concourse-driver-php/init-repo.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Initialize this repo
-
-# Ensure the script operates from the project root directory
-cd "${0%/*}"
-
-# Install the composer dependencies
-./composer.phar install
