8c9e96b7a264aacd11456e7a22a955ffe2103260
Jeff Nelson <jeff@cinchapi.com>
2015-10-24 09:42:04 -0400

    Update init.sh
    Fix init.sh to operate from any directory and properly delete itself

--- init.sh
+++ init.sh
@@ -3,12 +3,13 @@
 
 #Ensure that this script operates from the directory in which it resides
 cd "$(dirname "$0")"
+this=`basename "$0"`
 
 printf "sonatypeUsername=\nsonatypePassword=" > gradle.properties
-git update-index --assume-unchanged $0
+git update-index --assume-unchanged $this
 git update-index --assume-unchanged concourse-server/launch/Start\ Concourse.launch
 git update-index --assume-unchanged concourse-server/launch/Stop\ Concourse.launch
 git update-index --assume-unchanged concourse-shell/launch/Launch\ CaSH.launch
 ./utils/install-git-hooks.sh
-rm $0
+rm $this
 exit 0
