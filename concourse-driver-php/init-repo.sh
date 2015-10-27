a636d145682a218fdd5eb4ad47fcfdd4007dd58a
Jeff Nelson <jeff@cinchapi.com>
2015-10-27 06:44:27 -0400

    php-driver: make init-repo.sh delete itself after it runs

--- concourse-driver-php/init-repo.sh
+++ concourse-driver-php/init-repo.sh
@@ -3,6 +3,12 @@
 
 # Ensure the script operates from the project root directory
 cd "${0%/*}"
+this=`basename "$0"`
 
 # Install the composer dependencies
 ./composer.phar install
+
+# Delete this script so it isn't run again
+git update-index --assume-unchanged $this
+rm $this
+exit 0
