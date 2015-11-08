e3e2bd58bdda72c5d47d61965ffd8c605de284d4
Jeff Nelson <jeff@cinchapi.com>
2015-11-08 18:03:41 -0500

    add license headers to util scripts

--- utils/install-git-hooks.sh
+++ utils/install-git-hooks.sh
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
 # A script to install version controlled Git hooks into the local git repo
 . "`dirname "$0"`/.include"
 
