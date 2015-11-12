2a113a83080dae8e96d0422a86670961f13937c5
Jeff Nelson <jeff@cinchapi.com>
2015-11-12 10:57:02 -0500

    Add smart installer building in ConcourseCodebase
    Add logic to the ConcourseCodebase class to detect when the code is changed.
    This functionality is used by the ETE test framework to determine if the creation
    of a new build is actually necessary.
    
    Not creating installers when we don't have to will greatly speed up repeated ETE
    tests.

--- concourse-ete-test-core/src/main/java/com/cinchapi/concourse/util/ConcourseCodebase.java
+++ concourse-ete-test-core/src/main/java/com/cinchapi/concourse/util/ConcourseCodebase.java
@@ -18,6 +18,8 @@ package com.cinchapi.concourse.util;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Set;
 
@@ -164,6 +166,42 @@ public class ConcourseCodebase {
     }
 
     /**
+     * Return the path to the installer file for this codebase or an empty
+     * string if no installer file exists.
+     * 
+     * @return the path to the installer file
+     */
+    private String getInstallerPath() {
+        try {
+            String cmd = new StringBuilder().append("ls -a ").append(path)
+                    .append("/concourse-server/build/distributions | grep bin")
+                    .toString();
+            Process p = Processes.getBuilderWithPipeSupport(cmd).start();
+            Processes.waitForSuccessfulCompletion(p);
+            String installer = Processes.getStdOut(p).get(0);
+            if(!installer.isEmpty()) {
+                installer = path + "/concourse-server/build/distributions/"
+                        + installer;
+            }
+            return installer;
+        }
+        catch (Exception e) {
+            throw Throwables.propagate(e);
+        }
+    }
+
+    /**
+     * Return {@code true} if the codebase has an installer file, {@code false}
+     * otherwise.
+     * 
+     * @return a boolean that indicates whether the codebase has an installer
+     *         file or not
+     */
+    private boolean hasInstaller() {
+        return !getInstallerPath().isEmpty();
+    }
+
+    /**
      * Create a concourse-server.bin installer from this
      * {@link ConcourseCodebase codebase} and return the path to the installer
      * file.
@@ -172,21 +210,49 @@ public class ConcourseCodebase {
      */
     public String buildInstaller() {
         try {
-            Process p;
-            p = Processes.getBuilder("bash", "gradlew", "clean", "installer")
-                    .directory(new File(path)).start();
-            Processes.waitForSuccessfulCompletion(p);
-            String cmd = new StringBuilder().append("ls -a ").append(path)
-                    .append("/concourse-server/build/distributions | grep bin")
-                    .toString();
-            p = Processes.getBuilderWithPipeSupport(cmd).start();
-            Processes.waitForSuccessfulCompletion(p);
-            String installer = Processes.getStdOut(p).get(0);
-            installer = path + "/concourse-server/build/distributions/"
-                    + installer;
-            return installer;
+            if(!hasInstaller() || hasCodeChanged()) {
+                Process p = Processes
+                        .getBuilder("bash", "gradlew", "clean", "installer")
+                        .directory(new File(path)).start();
+                Processes.waitForSuccessfulCompletion(p);
+            }
+            return getInstallerPath();
         }
         catch (Exception e) {
+            throw Throwables.propagate(e);
+        }
+    }
+
+    /**
+     * The name of the file that contains a cache of the state of the codebase.
+     */
+    private static String CODE_STATE_CACHE_FILENAME = ".codestate";
+
+    /**
+     * Check to see if there has been a code change since the last check.
+     * 
+     * @return {@code true} if it appears that the code has changed
+     */
+    private boolean hasCodeChanged() {
+        Path cache = Paths.get(getPath(), CODE_STATE_CACHE_FILENAME)
+                .toAbsolutePath();
+        String cmd = Platform.isMacOsX() ? "(git status; git diff) | md5"
+                : "(git status; git diff) | md5sum";
+        try {
+            Process p = Processes.getBuilderWithPipeSupport(cmd)
+                    .directory(new File(getPath())).start();
+            Processes.waitForSuccessfulCompletion(p);
+            String state = Processes.getStdOut(p).get(0);
+            FileOps.touch(cache.toString());
+            String cached = FileOps.read(cache.toString());
+            boolean changed = false;
+            if(!state.equals(cached)) {
+                FileOps.write(state, cache.toString());
+                changed = true;
+            }
+            return changed;
+        }
+        catch (IOException e) {
             throw Throwables.propagate(e);
         }
     }
