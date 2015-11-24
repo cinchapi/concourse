18a5e51b3c8671dd23879706d2c3651c53532aca
Jeff Nelson <jeff@cinchapi.com>
2015-11-24 13:04:54 -0600

    update documentation for BuildableState and BuildableStartState to add clarity about their purposes

--- concourse-driver-java/src/main/java/com/cinchapi/concourse/lang/BuildableStartState.java
+++ concourse-driver-java/src/main/java/com/cinchapi/concourse/lang/BuildableStartState.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2013-2015 Cinchapi Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,7 +18,8 @@ package com.cinchapi.concourse.lang;
 /**
  * A {@link StartState} that is also a {@link BuildableState}. This distinction
  * is important so that we don't allow the caller to build an empty criteria or
- * a criteria with a hanging conjunction.
+ * a criteria with a hanging conjunction (because those conditions would simply
+ * place the builder in a {@link StateState} which is un-buildable).
  * 
  * @author Jeff Nelson
  */
--- concourse-driver-java/src/main/java/com/cinchapi/concourse/lang/BuildableState.java
+++ concourse-driver-java/src/main/java/com/cinchapi/concourse/lang/BuildableState.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2013-2015 Cinchapi Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,8 +16,8 @@
 package com.cinchapi.concourse.lang;
 
 /**
- * The base class for a state that can build a complete and well-formed
- * {@link Criteria}.
+ * The base class for a language state that can be transformed into a complete
+ * and well-formed {@link Criteria}.
  * 
  * @author Jeff Nelson
  */
@@ -41,7 +41,7 @@ public abstract class BuildableState extends State {
         criteria.close();
         return criteria;
     }
-    
+
     /**
      * Build a conjunctive clause onto the {@link Criteria} that is building.
      * 
