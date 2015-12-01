82a7af69d8d7c9650a334e345ffb0ee027c92c50
Jeff Nelson <jeff@cinchapi.com>
2015-12-01 08:53:32 -0500

    Fix rare race condition in CounterTest
    CounterTest would occasionally fail because performing the verifyAndSwap
    was not synchornized with adding the count to the list. This change
    should fix that

--- concourse-integration-tests/src/test/java/com/cinchapi/concourse/CounterTest.java
+++ concourse-integration-tests/src/test/java/com/cinchapi/concourse/CounterTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2013-2015 Cinchapi Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,9 +15,10 @@
  */
 package com.cinchapi.concourse;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -37,8 +38,8 @@ public class CounterTest extends ConcourseIntegrationTest {
 
     AtomicBoolean running = new AtomicBoolean(true);
     long stoptime = 0;
-    List<Integer> counts = Collections.synchronizedList(Lists
-            .<Integer> newArrayList());
+    List<Integer> counts = Lists.newArrayList();
+    Lock lock = new ReentrantLock();
 
     @Test
     public void test() throws InterruptedException {
@@ -88,12 +89,15 @@ public class CounterTest extends ConcourseIntegrationTest {
         public void run() {
             while (running.get()) {
                 int count = client.get("count", 1);
-                if(client.verifyAndSwap("count", count, 1, count + 1)) {
-                    counts.add(count + 1);
+                synchronized (counts) {
+                    if(client.verifyAndSwap("count", count, 1, count + 1)) {
+                        counts.add(count + 1);
+                    }
+                    else {
+                        // just try again...
+                    }
                 }
-                else {
-                    // just try again...
-                }
+
             }
             client.exit();
 
