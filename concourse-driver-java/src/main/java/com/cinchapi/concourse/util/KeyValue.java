c266638b12a45c4c1f9be213a8eab3a516ea0c1d
Jeff Nelson <jeff@cinchapi.com>
2015-12-06 09:08:04 -0500

    Add KeyValue class to encapsulate key/value pairs with intuitive semantics

---
+++ concourse-driver-java/src/main/java/com/cinchapi/concourse/util/KeyValue.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2013-2015 Cinchapi Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cinchapi.concourse.util;
+
+import java.util.AbstractMap.SimpleEntry;
+
+/**
+ * A simple association between two objects where one "maps" to the other.
+ * 
+ * @author Jeff Nelson
+ */
+public class KeyValue<K, V> extends SimpleEntry<K, V> {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Construct a new instance.
+     * 
+     * @param key the mapping key
+     * @param value the mapping value
+     */
+    public KeyValue(K key, V value) {
+        super(key, value);
+    }
+
+}
