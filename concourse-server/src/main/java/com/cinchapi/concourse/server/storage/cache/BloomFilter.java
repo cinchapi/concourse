219abfd2fbd19941afcfee83ea19ad0373e7b9e8
Jeff Nelson <jeff@cinchapi.com>
2016-05-07 10:50:16 -0400

    move Serializables util class to concourse-driver-java

--- concourse-server/src/main/java/com/cinchapi/concourse/server/storage/cache/BloomFilter.java
+++ concourse-server/src/main/java/com/cinchapi/concourse/server/storage/cache/BloomFilter.java
@@ -31,8 +31,8 @@ import jsr166e.StampedLock;
 import com.cinchapi.concourse.server.io.Byteable;
 import com.cinchapi.concourse.server.io.Composite;
 import com.cinchapi.concourse.server.io.FileSystem;
-import com.cinchapi.concourse.server.io.Serializables;
 import com.cinchapi.concourse.server.io.Syncable;
+import com.cinchapi.concourse.util.Serializables;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Throwables;
 
