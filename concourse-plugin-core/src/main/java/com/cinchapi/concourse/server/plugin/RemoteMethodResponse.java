3ba84180dafec0170f5957801173b6ab06b79b6b
Jeff Nelson <jeff@cinchapi.com>
2016-05-15 19:26:47 -0400

    doc update

--- concourse-plugin-core/src/main/java/com/cinchapi/concourse/server/plugin/RemoteMethodResponse.java
+++ concourse-plugin-core/src/main/java/com/cinchapi/concourse/server/plugin/RemoteMethodResponse.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2013-2016 Cinchapi Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,22 +20,30 @@ import java.io.Serializable;
 import com.cinchapi.concourse.thrift.TObject;
 
 /**
- * 
+ * A message that is sent from one process to another via a {@link SharedMemory}
+ * segment with the result of a {@link RemoteMethodInvocation}.
  * 
  * @author Jeff Nelson
  */
-public class RemoteMethodResponse implements Serializable{
+public class RemoteMethodResponse implements Serializable {
 
     /**
-     * 
+     * The serial version UID..
      */
     private static final long serialVersionUID = -7985973870612594547L;
-    
+
+    /**
+     * The response encapsulated as a thrift serializable object.
+     */
     public final TObject response;
-    
-    public RemoteMethodResponse(TObject response){
+
+    /**
+     * Construct a new instance.
+     * 
+     * @param response
+     */
+    public RemoteMethodResponse(TObject response) {
         this.response = response;
     }
 
 }
- 
