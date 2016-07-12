156170aa6298acdafdfdf39eb68b797af4bc029f
Jeff Nelson <jeff@cinchapi.com>
2016-07-12 01:04:07 -0400

    hack to make CORS work properly
    This commit manually allows all origins and all headers. This should
    eventually be made configurable once we come up with a systemtatic way
    of having options requests to any route automatically return something
    useful...at the very least it should return the methods that are allowed

--- concourse-server/src/main/java/spark/webserver/JettyHandler.java
+++ concourse-server/src/main/java/spark/webserver/JettyHandler.java
@@ -196,8 +196,12 @@ class JettyHandler extends SessionHandler {
             throws IOException, ServletException {
         try {
             rewrite(target, baseRequest, request);
-            filter.doFilter(request, response, null);
+            if(!request.getMethod().equalsIgnoreCase("OPTIONS")){
+                filter.doFilter(request, response, null);
+            }
             baseRequest.setHandled(true);
+            response.addHeader("Access-Control-Allow-Origin", "*");
+            response.addHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"));
         }
         catch (NotConsumedException ignore) {
             // TODO : Not use an exception in order to be faster.
