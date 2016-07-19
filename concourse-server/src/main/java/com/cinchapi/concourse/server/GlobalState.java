031e586a040f9840abd99852225a7e622dfe5b3e
Aditya Srinivasan <as577@duke.edu>
2016-07-19 21:31:29 +0530

    Reverted changes to GlobalState.java

--- concourse-server/src/main/java/com/cinchapi/concourse/server/GlobalState.java
+++ concourse-server/src/main/java/com/cinchapi/concourse/server/GlobalState.java
@@ -177,9 +177,16 @@ public final class GlobalState extends Constants {
     static {
         ConcourseServerPreferences config;
         try {
-            String prefs = System.getProperty("com.cinchapi.concourse.server.prefs");
-            config = ConcourseServerPreferences.open(prefs);
-            PREFS_FILE_PATH = FileSystem.expandPath(prefs);
+            String devPrefs = "conf" + File.separator + "concourse.prefs.dev";
+            String defaultPrefs = "conf" + File.separator + "concourse.prefs";
+            if(FileSystem.hasFile(devPrefs)) {
+                config = ConcourseServerPreferences.open(devPrefs);
+                PREFS_FILE_PATH = FileSystem.expandPath(devPrefs);
+            }
+            else {
+                config = ConcourseServerPreferences.open(defaultPrefs);
+                PREFS_FILE_PATH = FileSystem.expandPath(defaultPrefs);
+            }
         }
         catch (Exception e) {
             config = null;
