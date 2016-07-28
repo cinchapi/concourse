3742927d8aac7a8bf289ae08814d6a6f043ff153
Aditya Srinivasan <asrinivasan@cinchapi.com>
2016-07-28 22:55:11 +0530

    Added headers and fixed formatting

--- concourse-plugin-core/src/main/java/com/cinchapi/concourse/plugin/data/TrackingMultimap.java
+++ concourse-plugin-core/src/main/java/com/cinchapi/concourse/plugin/data/TrackingMultimap.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2013-2016 Cinchapi Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.cinchapi.concourse.plugin.data;
 
 import java.util.AbstractMap;
@@ -211,11 +226,13 @@ public abstract class TrackingMultimap<K, V> extends AbstractMap<K, Set<V>> {
      * @return
      */
     public VariableType variableType() {
-        if (uniqueValueCount.get() <= 2) {
+        if(uniqueValueCount.get() <= 2) {
             return VariableType.DICHOTOMOUS;
-        } else if (uniqueValueCount.get() <= 12) {
+        }
+        else if(uniqueValueCount.get() <= 12) {
             return VariableType.NOMINAL;
-        } else {
+        }
+        else {
             return VariableType.INTERVAL;
         }
     }
