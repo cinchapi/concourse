e0d650cb7fbca3c85ce07f564ae8179d106a3ac6
Jeff Nelson <jeff@cinchapi.com>
2016-08-29 07:21:47 -0400

    add utility method to block while trying to drain a queue

--- concourse-driver-java/src/main/java/com/cinchapi/concourse/util/Queues.java
+++ concourse-driver-java/src/main/java/com/cinchapi/concourse/util/Queues.java
@@ -16,7 +16,13 @@
 package com.cinchapi.concourse.util;
 
 import java.util.ArrayDeque;
+import java.util.Collection;
 import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Throwables;
 
 /**
  * Static factory methods for returning objects that implement the {@link Queue}
@@ -33,6 +39,71 @@ public final class Queues {
      */
     public static <T> Queue<T> newSingleThreadedQueue() {
         return new ArrayDeque<T>();
+    }
+
+    /**
+     * Perform the {@link BlockingQueue#drainTo(Collection)} operation, but
+     * block indefinitely until at least one element is available.
+     * 
+     * @param queue the {@link BlockingQueue} in which to perform the
+     *            {@link BlockingQueue#drainTo(Collection) drainTo} method
+     * @param buffer the collection into which the elements are drained
+     * @return the number of elements that are drained
+     */
+    public static <E> int blockingDrain(BlockingQueue<E> queue,
+            Collection<? super E> buffer) {
+        Preconditions.checkNotNull(buffer);
+        int added = queue.drainTo(buffer);
+        try {
+            if(added == 0) {
+                // If the initial drain doesn't return any elements, we must
+                // call
+                // #take in order to block until at least on element is
+                // available
+                buffer.add(queue.take());
+                added += queue.drainTo(buffer);
+                ++added;
+            }
+        }
+        catch (InterruptedException e) {
+            throw Throwables.propagate(e);
+        }
+        return added;
+    }
+
+    /**
+     * Perform the {@link BlockingQueue#drainTo(Collection)} operation, but
+     * block for the specified {@code timeout} until at least one element is
+     * available.
+     * 
+     * @param queue the {@link BlockingQueue} in which to perform the
+     *            {@link BlockingQueue#drainTo(Collection) drainTo} method
+     * @param buffer the collection into which the elements are drained
+     * @return the number of elements that are drained
+     */
+    public static <E> int blockingDrain(BlockingQueue<E> queue,
+            Collection<? super E> buffer, long timeout, TimeUnit unit) {
+        Preconditions.checkNotNull(buffer);
+        long deadline = System.nanoTime() + unit.toNanos(timeout);
+        int added = queue.drainTo(buffer);
+        try {
+            if(added == 0) {
+                E element = queue.poll(deadline - System.nanoTime(),
+                        TimeUnit.NANOSECONDS);
+                if(element != null) {
+                    // If the element is null, it means we've waited long enough
+                    // and no element has been added; otherwise, we need to
+                    // check if more than one element was added
+                    buffer.add(element);
+                    added += queue.drainTo(buffer);
+                    ++added;
+                }
+            }
+        }
+        catch (InterruptedException e) {
+            throw Throwables.propagate(e);
+        }
+        return added;
     }
 
     private Queues() {/* noop */}
