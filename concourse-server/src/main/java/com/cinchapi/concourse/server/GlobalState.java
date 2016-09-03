bbfbb7ea078210e2a7fb65b9429d02747bc7d8b3
Jeff Nelson <jeff@cinchapi.com>
2016-09-02 21:42:55 -0400

    use Reflection class from concourse-driver-java instead of accent4j

--- concourse-server/src/main/java/com/cinchapi/concourse/server/GlobalState.java
+++ concourse-server/src/main/java/com/cinchapi/concourse/server/GlobalState.java
@@ -24,13 +24,13 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 import javax.annotation.Nullable;
 
-import com.cinchapi.common.reflect.Reflection;
 import com.cinchapi.concourse.Constants;
 import com.cinchapi.concourse.annotate.NonPreference;
 import com.cinchapi.concourse.config.ConcourseServerPreferences;
 import com.cinchapi.concourse.server.io.FileSystem;
 import com.cinchapi.concourse.server.plugin.model.WriteEvent;
 import com.cinchapi.concourse.util.Networking;
+import com.cinchapi.concourse.util.Reflection;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Throwables;
 import com.google.common.collect.Sets;
@@ -208,13 +208,13 @@ public final class GlobalState extends Constants {
      * </p>
      */
     public static Level LOG_LEVEL = Level.INFO;
-    
+
     /**
      * The class representation of {@link RemoteInvocationThread}.
      */
     @NonPreference
-    public static final Class<?> INVOCATION_THREAD_CLASS = Reflection.getClassCasted("com.cinchapi.concourse.server.plugin.RemoteInvocationThread");
-
+    public static final Class<?> INVOCATION_THREAD_CLASS = Reflection
+            .getClassCasted("com.cinchapi.concourse.server.plugin.RemoteInvocationThread");
 
     /**
      * Whether log messages should also be printed to the console.
