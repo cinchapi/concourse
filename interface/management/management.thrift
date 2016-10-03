44cfb3a5ff67cf78079811a4fe48a7e286c4d8cb
Kylych Tynybekov <ktynybekov@gmail.com>
2016-10-03 22:06:09 +0600

    CON-528: Added -License -managementLogin function that logs in user and return AccessToken

--- interface/management/management.thrift
+++ interface/management/management.thrift
@@ -1,8 +1,42 @@
+# Copyright (c) 2013-2016 Cinchapi Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Interface definition for the Concourse Management Server API.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 include "../shared.thrift"
 
 namespace java com.cinchapi.concourse.server.management
 
 service ConcourseManagementService {
+
+    /**
+     * Return {@code AccessToken} if {@code username} and {@code password} is a valid
+     * combination to login to the server for the purpose of performing a
+     * managed operation. This method should only be used to authenticate a user
+     * for the purpose of performing a single operation.
+     *
+     * @param username
+     * @param password
+     * @return {@link AccessToken} if the credentials are valid
+     */
+
+    shared.AccessToken managementLogin(
+     1: binary username,
+     2: binary password);
 
    /**
      * Disable the user(i.e. the user cannot be authenticated for any purposes,
