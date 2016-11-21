7ae2f6232f8b234c2a80de7361dc4d6bfa094f4e
Jeff Nelson <jeff@cinchapi.com>
2016-11-21 10:25:15 -0800

    fix unit tests

--- concourse-integration-tests/src/test/java/com/cinchapi/concourse/SumTest.java
+++ concourse-integration-tests/src/test/java/com/cinchapi/concourse/SumTest.java
@@ -1,6 +1,5 @@
 package com.cinchapi.concourse;
 
-
 import java.util.List;
 
 import org.junit.Assert;
@@ -11,7 +10,6 @@ import com.cinchapi.concourse.lang.Criteria;
 import com.cinchapi.concourse.test.ConcourseIntegrationTest;
 import com.cinchapi.concourse.thrift.Operator;
 
-
 /**
  * Tests to check the functionality of sum feature.
  * 
@@ -19,95 +17,6 @@ import com.cinchapi.concourse.thrift.Operator;
  */
 public class SumTest extends ConcourseIntegrationTest {
 
-    @Test
-    public void testSumKeyCcl() {
-        String key = "age";
-        client.add("name", "foo", 1);
-        client.add(key, 30, 1);
-        client.add("name", "bar", 2);
-        client.add(key, 19, 2);
-        client.add("name", "bar", 2);
-        client.add(key, 15, 2);
-        int actual = 34;
-        Number expected = client.calculate().sum(key, "name = bar");
-        Assert.assertEquals(expected.intValue(), actual);
-    }
-
-    @Test
-    public void testSumKeyRecordTime() {
-        String key = "age";
-        client.add("name", "foo", 1);
-        client.add(key, 30, 1);
-        client.add(key, 19, 1);
-        int actual = 49;
-        Number expected = client.calculate().sum(key, 1, Timestamp.now());
-        Assert.assertEquals(expected.intValue(), actual);
-    }
-    
-    @Test
-    public void testSumKeyCclTime() {
-        String key = "age";
-        client.add("name", "foo", 1);
-        client.add(key, 30, 1);
-        client.add("name", "bar", 2);
-        client.add(key, 19, 2);
-        client.add("name", "bar", 2);
-        client.add(key, 15, 2);
-        Number actual = 34;
-        Number expected = client.calculate().sum(key, "name = bar", Timestamp.now());
-        Assert.assertEquals(expected.intValue(), actual.intValue());
-    }
-    
-    @Test
-    public void testSumKeyCriteria() {
-        String key = "age";
-        client.add("name", "foo", 1);
-        client.add(key, 30, 1);
-        client.add("name", "bar", 2);
-        client.add(key, 19, 2);
-        client.add("name", "bar", 2);
-        client.add(key, 15, 2);
-        int actual = 34;
-        Number expected = client.calculate().sum(key, Criteria.where().key("age")
-                .operator(Operator.LESS_THAN).value(20).build());
-        Assert.assertEquals(expected.intValue(), actual);
-    }
-    
-    @Test
-    public void testSumKeyCriteriaTime() {
-        String key = "age";
-        client.add("name", "foo", 1);
-        client.add(key, 30, 1);
-        client.add("name", "bar", 2);
-        client.add(key, 19, 2);
-        client.add("name", "bar", 2);
-        client.add(key, 15, 2);
-        Number actual = 34;
-        Number expected = client.calculate().sum(key, Criteria.where().key("age")
-                .operator(Operator.LESS_THAN).value(20).build(), Timestamp.now());
-        Assert.assertEquals(expected.intValue(), actual.intValue());
-    }
-   
-    @Test
-    public void testSumKeyRecord() {
-        String key = "age";
-        client.add("name", "foo", 1);
-        client.add(key, 30, 1);
-        client.add(key, 19, 1);
-        int actual = 49;
-        Number expected = client.calculate().sum(key, 1);
-        Assert.assertEquals(expected.intValue(), actual);
-    }
-    
-    @Test(expected = RuntimeException.class)
-    public void testSumKeyRecordException() {
-        String key = "age";
-        client.add("name", "foo", 1);
-        client.add(key, 30, 1);
-        client.add(key, "fifteen", 1);
-        client.calculate().sum(key, 1);
-    }
-    
     @Test
     public void testSumKey() {
         String key = "age";
@@ -121,7 +30,71 @@ public class SumTest extends ConcourseIntegrationTest {
         Number expected = client.calculate().sum(key);
         Assert.assertEquals(expected.intValue(), actual);
     }
-    
+
+    @Test
+    public void testSumKeyCcl() {
+        String key = "age";
+        client.add("name", "foo", 1);
+        client.add(key, 30, 1);
+        client.add("name", "bar", 2);
+        client.add(key, 19, 2);
+        client.add("name", "bar", 2);
+        client.add(key, 15, 2);
+        int actual = 34;
+        Number expected = client.calculate().sum(key, "name = bar");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSumKeyCclTime() {
+        String key = "age";
+        client.add("name", "foo", 1);
+        client.add(key, 30, 1);
+        client.add("name", "bar", 2);
+        client.add(key, 19, 2);
+        client.add("name", "bar", 2);
+        client.add(key, 15, 2);
+        int actual = 34;
+        Timestamp timestamp = Timestamp.now();
+        client.add(key, 100, 2);
+        Number expected = client.calculate().sum(key, "name = bar", timestamp);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSumKeyCriteria() {
+        String key = "age";
+        client.add("name", "foo", 1);
+        client.add(key, 30, 1);
+        client.add("name", "bar", 2);
+        client.add(key, 19, 2);
+        client.add("name", "bar", 2);
+        client.add(key, 15, 2);
+        int actual = 34;
+        Number expected = client.calculate().sum(key, Criteria.where()
+                .key("age").operator(Operator.LESS_THAN).value(20).build());
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSumKeyCriteriaTime() {
+        String key = "age";
+        client.add("name", "foo", 1);
+        client.add(key, 30, 1);
+        client.add("name", "bar", 2);
+        client.add(key, 19, 2);
+        client.add("name", "bar", 2);
+        client.add(key, 15, 2);
+        int actual = 34;
+        Timestamp timestamp = Timestamp.now();
+        client.add(key, 100, 2);
+        Number expected = client.calculate()
+                .sum(key, Criteria.where().key("age")
+                        .operator(Operator.LESS_THAN).value(20).build(),
+                        timestamp);
+        Assert.assertEquals(expected, actual);
+    }
+
     @Test(expected = RuntimeException.class)
     public void testSumKeyException() {
         String key = "age";
@@ -132,32 +105,27 @@ public class SumTest extends ConcourseIntegrationTest {
         client.add(key, 15, 2);
         client.calculate().sum(key);
     }
-    
+
     @Test
-    public void testSumKeyTime() {
+    public void testSumKeyRecord() {
         String key = "age";
         client.add("name", "foo", 1);
         client.add(key, 30, 1);
-        client.add("name", "bar", 2);
-        client.add(key, 19, 2);
-        client.add("name", "bar", 2);
-        client.add(key, 15, 2);
-        int actual = 64;
-        Number expected = client.calculate().sum(key, Timestamp.now());
-        Assert.assertEquals(expected.intValue(), actual);
+        client.add(key, 19, 1);
+        int actual = 49;
+        Number expected = client.calculate().sum(key, 1);
+        Assert.assertEquals(expected, actual);
     }
-    
+
     @Test(expected = RuntimeException.class)
-    public void testSumKeyTimeException() {
+    public void testSumKeyRecordException() {
         String key = "age";
         client.add("name", "foo", 1);
         client.add(key, 30, 1);
         client.add(key, "fifteen", 1);
-        client.add("name", "bar", 2);
-        client.add(key, 15, 2);
-        client.calculate().sum(key, Timestamp.now());
+        client.calculate().sum(key, 1);
     }
-    
+
     @Test
     public void testSumKeyRecords() {
         String key = "age";
@@ -169,10 +137,10 @@ public class SumTest extends ConcourseIntegrationTest {
         List<Long> list = Lists.newArrayList();
         list.add((long) 1);
         list.add((long) 2);
-        Number expected = client.calculate().sum(key,list);
-        Assert.assertEquals(expected.intValue(), actual);
+        Number expected = client.calculate().sum(key, list);
+        Assert.assertEquals(expected, actual);
     }
-    
+
     @Test(expected = RuntimeException.class)
     public void testSumKeyRecordsException() {
         String key = "age";
@@ -183,9 +151,9 @@ public class SumTest extends ConcourseIntegrationTest {
         List<Long> list = Lists.newArrayList();
         list.add((long) 1);
         list.add((long) 2);
-        client.calculate().sum(key,list);
+        client.calculate().sum(key, list);
     }
-    
+
     @Test
     public void testSumKeyRecordsTime() {
         String key = "age";
@@ -194,22 +162,62 @@ public class SumTest extends ConcourseIntegrationTest {
         client.add("name", "bar", 2);
         client.add(key, 20, 2);
         int actual = 50;
-        List<Long> list = Lists.newArrayList();
-        list.add((long) 1);
-        list.add((long) 2);
-        Number expected = client.calculate().sum(key,list, Timestamp.now());
+        Timestamp timestamp = Timestamp.now();
+        client.add(key, 100, 2);
+        Number expected = client.calculate().sum(key,
+                Lists.newArrayList(1L, 2L), timestamp);
         Assert.assertEquals(expected.intValue(), actual);
     }
-    
+
+    @Test(expected = RuntimeException.class)
     public void testSumKeyRecordsTimeException() {
         String key = "age";
         client.add("name", "foo", 1);
         client.add(key, 30, 1);
         client.add("name", "bar", 2);
         client.add(key, "fifty", 2);
-        List<Long> list = Lists.newArrayList();
-        list.add((long) 1);
-        list.add((long) 2);
-        client.calculate().sum(key,list, Timestamp.now());
+        Timestamp timestamp = Timestamp.now();
+        client.add(key, 100, 2);
+        client.calculate().sum(key, Lists.newArrayList(1L, 2L), timestamp);
+    }
+
+    @Test
+    public void testSumKeyRecordTime() {
+        String key = "age";
+        client.add("name", "foo", 1);
+        client.add(key, 30, 1);
+        client.add(key, 19, 1);
+        int actual = 49;
+        Timestamp timestamp = Timestamp.now();
+        client.add(key, 100);
+        Number expected = client.calculate().sum(key, 1, timestamp);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSumKeyTime() {
+        String key = "age";
+        client.add("name", "foo", 1);
+        client.add(key, 30, 1);
+        client.add("name", "bar", 2);
+        client.add(key, 19, 2);
+        client.add("name", "bar", 2);
+        client.add(key, 15, 2);
+        int actual = 64;
+        Timestamp timestamp = Timestamp.now();
+        client.add(key, 100, 2);
+        Number expected = client.calculate().sum(key, timestamp);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testSumKeyTimeException() {
+        String key = "age";
+        client.add("name", "foo", 1);
+        client.add(key, 30, 1);
+        client.add(key, "fifteen", 1);
+        client.add("name", "bar", 2);
+        client.add(key, 15, 2);
+        client.calculate().sum(key, Timestamp.now());
     }
 }
