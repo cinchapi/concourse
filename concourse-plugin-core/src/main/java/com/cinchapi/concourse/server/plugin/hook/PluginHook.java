9dac8dd7413403e6bc7765b38aceffc03c24fbe9
Jeff Nelson <jeff@cinchapi.com>
2016-12-06 10:39:59 -1000

    Clarify intention of plugin hooks
    1. Add a parent PluginHook class
    2. Make AfterInstallHook extend PluginHook
    3. Make the run method of PluginHooks return void instead of a boolean
    4. Update docs to make it clear that the hook should throw an exception if there is an error
    5. Update the PluginManager to not expect a boolean when invoking a hook

--- concourse-plugin-core/src/main/java/com/cinchapi/concourse/server/plugin/hook/PluginHook.java
+++ concourse-plugin-core/src/main/java/com/cinchapi/concourse/server/plugin/hook/PluginHook.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2013-2016 Cinchapi Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,21 +18,24 @@ package com.cinchapi.concourse.server.plugin.hook;
 import com.cinchapi.concourse.server.plugin.PluginContext;
 
 /**
- * A hook that is run <strong>after</strong> the plugin is installed.
+ * Parent interface for all plugin hooks.
  * 
  * @author Jeff Nelson
  */
-@FunctionalInterface
-public interface AfterInstallHook extends PluginHook{
+public interface PluginHook {
 
     /**
-     * Given the newly installed plugin's {@code context}, execute additional
-     * pre-launch logic. If this method return {@code false} the plugin
-     * installation will rollback. Otherwise, the plugin's installation will be
-     * finalized and the plugin will launch.
+     * Given the installation {@link PluginContext context} for a plugin,
+     * execute a hook to configure its runtime behavior.
+     * <p>
+     * The hook is assumed to have run successfully if this method terminates
+     * without exception. Whenever an exception is thrown by a hook, the parent
+     * process that invokes the hook (i.e installation or starting the plugin,
+     * etc) will fail and Concourse Server will communicate the failure to the
+     * user.
+     * </p>
      * 
      * @param context the plugin's {@link PluginContext context}
-     * @return {@code true} if the plugin's installation can be finalized
      */
     public void run(PluginContext context);
 
