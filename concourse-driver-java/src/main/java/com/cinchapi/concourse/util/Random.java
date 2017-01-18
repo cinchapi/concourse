b597e2c6b16657c0b6e1d6d35a330eff8a4e92f8
Jeff Nelson <jeff@cinchapi.com>
2017-01-18 09:07:54 -0500

    add microsleep method to Random

--- concourse-driver-java/src/main/java/com/cinchapi/concourse/util/Random.java
+++ concourse-driver-java/src/main/java/com/cinchapi/concourse/util/Random.java
@@ -161,7 +161,19 @@ public abstract class Random {
     }
 
     /**
-     * Pause execution for a random number of milliseconds between 0 and 1000 (1
+     * Pause execution for a random number of milliseconds between 1 and 50.
+     */
+    public static void microSleep() {
+        try {
+            Thread.sleep(50 + 1);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Pause execution for a random number of milliseconds between 1 and 1000 (1
      * second).
      */
     public static void sleep() {
