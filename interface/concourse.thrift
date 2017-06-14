fa786f6c20fb763de35657613a82caf46766c10f
Gerald Nash <g.nash.dev@gmail.com>
2017-06-13 22:01:39 -0400

    Changes to concourse.thrift

--- interface/concourse.thrift
+++ interface/concourse.thrift
@@ -1110,7 +1110,7 @@ service ConcourseService {
    *                      server uses to find the current transaction for the
    *                      client (optional)
    * @param environment the environment to which the client is connected
-   * @return the {@link Set} of keys in {@code record}
+   * @return the {@link Set} of keys in the database
    * @throws exceptions.SecurityException if the {@code creds} don't
    *         represent a valid session
    * @throws exceptions.TransactionException if the client was in a
@@ -1128,15 +1128,14 @@ service ConcourseService {
   /**
    * List all the keys in the database at {@code timestamp}.
    *
-   * @param record the record id
-   * @param timestamp the historical timestamp to use in the lookup
    * @param creds the {@link shared.AccessToken} that is used to authenticate
    *                the user on behalf of whom the client is connected
    * @param transaction the {@link shared.TransactionToken} that the
    *                      server uses to find the current transaction for the
    *                      client (optional)
    * @param environment the environment to which the client is connected
-   * @return the {@link Set} of keys that were in {@code record} at
+   * @param timestamp the historical timestamp to use in the lookup
+   * @return the {@link Set} of keys that were in the database at
    *         {@code timestamp}
    * @throws exceptions.SecurityException if the {@code creds} don't
    *         represent a valid session
@@ -1156,15 +1155,14 @@ service ConcourseService {
   /**
    * List all the keys in the database at {@code timestamp}.
    *
-   * @param record the record id
-   * @param timestamp the historical timestamp to use in the lookup
    * @param creds the {@link shared.AccessToken} that is used to authenticate
    *                the user on behalf of whom the client is connected
    * @param transaction the {@link shared.TransactionToken} that the
    *                      server uses to find the current transaction for the
    *                      client (optional)
    * @param environment the environment to which the client is connected
-   * @return the {@link Set} of keys that were in {@code record} at
+   * @param timestamp the historical timestamp to use in the lookup
+   * @return the {@link Set} of keys that were in the database at
    *         {@code timestamp}
    * @throws exceptions.SecurityException if the {@code creds} don't
    *         represent a valid session
