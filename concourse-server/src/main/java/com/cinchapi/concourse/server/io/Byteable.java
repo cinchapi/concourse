e56d4c8d34019afaf178402637a8788b77a0bcdb
Jeff Nelson <jeff@cinchapi.com>
2017-08-31 14:42:29 -0400

    add default impl for getBytes() that relies on copyTo

--- concourse-server/src/main/java/com/cinchapi/concourse/server/io/Byteable.java
+++ concourse-server/src/main/java/com/cinchapi/concourse/server/io/Byteable.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -80,7 +80,12 @@ public interface Byteable {
      * 
      * @return the byte sequence.
      */
-    public ByteBuffer getBytes();
+    public default ByteBuffer getBytes() {
+        ByteBuffer bytes = ByteBuffer.allocate(size());
+        copyTo(bytes);
+        bytes.rewind();
+        return bytes;
+    }
 
     /**
      * Returns the total number of bytes used to represent this object.
