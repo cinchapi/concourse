cd6d2bdae599b8d069ce2264319875e34a7d859d
Jeff Nelson <jeff@cinchapi.com>
2017-11-07 12:32:14 -0500

    CLOUD-1: Add access token check to invokePlugin method (#299)
    * add unit tests to repro CLOUD-1
    
    * CON-590: Ignore SecurityException thrown on ManagementCli exit
    
    This commit adds logic to ignore a SecurityException being raised in the
    exit method of a ManagementCli. The exception happens because the token
    that was being using in the CLI has become invalid. It is safe to ignore
    the token on exit because the work that requires authorization has already
    been completed.
    
    * add correctness check to unit test
    
    * CLOUD-1: Add access token check to invokePlugin method
    
    This was eroneously left off so there were some issues with plugin method
    call validation.

--- concourse-plugin-core-tests/src/test/java/com/cinchapi/concourse/server/plugin/TestPlugin.java
+++ concourse-plugin-core-tests/src/test/java/com/cinchapi/concourse/server/plugin/TestPlugin.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 package com.cinchapi.concourse.server.plugin;
+
+import java.util.Set;
 
 import ch.qos.logback.classic.Level;
 
@@ -26,29 +28,32 @@ public class TestPlugin extends Plugin {
 
     /**
      * Construct a new instance.
+     * 
      * @param fromServer
      * @param fromPlugin
      */
     public TestPlugin(String fromServer, String fromPlugin) {
         super(fromServer, fromPlugin);
     }
-    
-    public String echo(String input){
+
+    public String echo(String input) {
         return input;
+    }
+
+    public Set<Long> inventory() {
+        return runtime.inventory();
     }
 
     @Override
     protected PluginConfiguration getConfig() {
-        return new StandardPluginConfiguration(){
+        return new StandardPluginConfiguration() {
 
             @Override
-            public Level getLogLevel() { 
+            public Level getLogLevel() {
                 return Level.DEBUG;
             }
-            
+
         };
     }
-    
-    
 
 }
