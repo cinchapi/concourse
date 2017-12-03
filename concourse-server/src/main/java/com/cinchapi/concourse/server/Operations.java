46d917baa79ae7a0fc6182134487837e549f4e96
Jeff Nelson <jtnelson1@gmail.com>
2017-12-03 12:17:07 -0800

    improve syntax

--- concourse-server/src/main/java/com/cinchapi/concourse/server/Operations.java
+++ concourse-server/src/main/java/com/cinchapi/concourse/server/Operations.java
@@ -109,7 +109,8 @@ final class Operations {
     public static Number avgKeyAtomic(String key, long timestamp,
             AtomicOperation atomic) {
         Map<TObject, Set<Long>> data = timestamp == Time.NONE
-                ? atomic.browse(key) : atomic.browse(key, timestamp);
+                ? atomic.browse(key)
+                : atomic.browse(key, timestamp);
         Number avg = 0;
         int count = 0;
         for (Entry<TObject, Set<Long>> entry : data.entrySet()) {
@@ -351,7 +352,7 @@ final class Operations {
                     else {
                         throw new IllegalArgumentException(
                                 "Cannot query on record id using "
-                                        + expression.operator().operator());
+                                        + expression.raw().operator());
                     }
                 }
                 else {
@@ -512,7 +513,8 @@ final class Operations {
         JsonArray array = new JsonArray();
         for (long record : records) {
             Map<String, Set<TObject>> data = timestamp == 0
-                    ? store.select(record) : store.select(record, timestamp);
+                    ? store.select(record)
+                    : store.select(record, timestamp);
             JsonElement object = DataServices.gson().toJsonTree(data);
             if(includeId) {
                 object.getAsJsonObject().addProperty(
@@ -835,7 +837,8 @@ final class Operations {
     private static Number calculateKeyAtomic(String key, long timestamp,
             Number result, AtomicOperation atomic, KeyCalculation calculation) {
         Map<TObject, Set<Long>> data = timestamp == Time.NONE
-                ? atomic.browse(key) : atomic.browse(key, timestamp);
+                ? atomic.browse(key)
+                : atomic.browse(key, timestamp);
         for (Entry<TObject, Set<Long>> entry : data.entrySet()) {
             TObject tobject = entry.getKey();
             Set<Long> records = entry.getValue();
