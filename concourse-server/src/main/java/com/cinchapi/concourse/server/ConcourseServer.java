e5f5f794d60b97282411bc459779c8ec2386888d
Jeff Nelson <jeff@cinchapi.com>
2017-12-15 14:02:10 -0600

    fix formatting

--- concourse-server/src/main/java/com/cinchapi/concourse/server/ConcourseServer.java
+++ concourse-server/src/main/java/com/cinchapi/concourse/server/ConcourseServer.java
@@ -129,8 +129,8 @@ import com.google.inject.matcher.Matchers;
  *
  * @author Jeff Nelson
  */
-public class ConcourseServer extends BaseConcourseServer implements
-        ConcourseService.Iface {
+public class ConcourseServer extends BaseConcourseServer
+        implements ConcourseService.Iface {
 
     /**
      * Contains the credentials used by the {@link #accessManager}. This file is
@@ -2183,8 +2183,8 @@ public class ConcourseServer extends BaseConcourseServer implements
         else {
             throw new DuplicateEntryException(
                     com.cinchapi.concourse.util.Strings.joinWithSpace("Found",
-                            records.size(), "records that match",
-                            Language.translateFromThriftCriteria(criteria)));
+                            records.size(), "records that match", Language
+                                    .translateFromThriftCriteria(criteria)));
         }
     }
 
