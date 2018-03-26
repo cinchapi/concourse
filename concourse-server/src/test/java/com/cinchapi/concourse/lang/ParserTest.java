5890d6573806ad4e5b41d0b874eebde26081b39f
Jeff Nelson <jeff@cinchapi.com>
2018-03-26 15:12:19 -0400

    Upgrade to CCL 2.3.3 (#330)
    * add unit test repro
    
    * upgrade CCL to 2.3.3
    
    * remove println
    
    * spotless

--- concourse-server/src/test/java/com/cinchapi/concourse/lang/ParserTest.java
+++ concourse-server/src/test/java/com/cinchapi/concourse/lang/ParserTest.java
@@ -748,4 +748,12 @@ public class ParserTest {
             }
         }
     }
+
+    @Test
+    public void testReproCCL_11() {
+        String ccl = "user LINKS_TO 1234";
+        Parser parser = Parsers.create(ccl);
+        parser.parse();
+        Assert.assertTrue(true); // lack of Exception means we pass
+    }
 }
