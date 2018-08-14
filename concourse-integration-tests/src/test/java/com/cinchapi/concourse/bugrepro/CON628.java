91b2d5f168f49c7922f7fc03936543c9db36321c
Jeff Nelson <jeff@cinchapi.com>
2018-08-13 21:39:17 -0400

    add repro unit test

---
+++ concourse-integration-tests/src/test/java/com/cinchapi/concourse/bugrepro/CON628.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2013-2018 Cinchapi Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cinchapi.concourse.bugrepro;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.cinchapi.concourse.Tag;
+import com.cinchapi.concourse.lang.Criteria;
+import com.cinchapi.concourse.test.ConcourseIntegrationTest;
+import com.cinchapi.concourse.thrift.Operator;
+import com.google.common.collect.ImmutableSet;
+
+/**
+ * Unit test to reproduce the bug described in CON-628.
+ *
+ * @author Jeff Nelson
+ */
+public class CON628 extends ConcourseIntegrationTest {
+
+    @Test
+    public void repro() {
+        long record = client.add("foo", Tag.create("17"));
+        Assert.assertEquals(ImmutableSet.of(record),
+                client.find(Criteria.where().key("foo")
+                        .operator(Operator.EQUALS).value(Tag.create("17"))));
+    }
+
+}
