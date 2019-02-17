06e08f4ea3de8413a14180a51464830b0ae64165
Jeff Nelson <jeff@jeffnelson.com>
2019-02-16 21:02:34 -0500

    CON-635: Add support for Criteria containing all Unicode quote characters (#357)
    * stage changes
    
    * upgrade CCL module to 2.5.2
    
    * javadoc update
    
    * update changelog

---
+++ concourse-integration-tests/src/test/java/com/cinchapi/concourse/bugrepro/CON635.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2013-2019 Cinchapi Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cinchapi.concourse.bugrepro;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.cinchapi.concourse.lang.Criteria;
+import com.cinchapi.concourse.test.ConcourseIntegrationTest;
+import com.cinchapi.concourse.thrift.Operator;
+
+/**
+ * Repro of http://jira.cinchapi.com/browse/CON-635
+ *
+ * @author Jeff Nelson
+ */
+public class CON635 extends ConcourseIntegrationTest {
+
+    @Test
+    public void repro1() {
+        client.find(Criteria.where().key("foo").operator(Operator.EQUALS)
+                .value("Foo’s"));
+        Assert.assertTrue(true); // lack of Exception means test passes
+    }
+
+    @Test
+    public void repor2() {
+        client.find(Criteria.where().key("foo").operator(Operator.EQUALS)
+                .value("“A and B”"));
+        Assert.assertTrue(true); // lack of Exception means test passes
+    }
+
+}
