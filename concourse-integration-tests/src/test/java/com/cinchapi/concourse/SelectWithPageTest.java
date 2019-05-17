1446177e50d6536a50e6b50e7ad604b1d93a2a67
javierlores <javier@cinchapi.com>
2019-05-17 14:22:31 -0400

    Added integration tests for page

--- concourse-integration-tests/src/test/java/com/cinchapi/concourse/SelectWithPageTest.java
+++ concourse-integration-tests/src/test/java/com/cinchapi/concourse/SelectWithPageTest.java
@@ -1,32 +1,41 @@
+/*
+ * Copyright (c) 2013-2019 Cinchapi Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.cinchapi.concourse;
 
-import com.cinchapi.common.base.CheckedExceptions;
 import com.cinchapi.concourse.importer.CsvImporter;
 import com.cinchapi.concourse.importer.Importer;
 import com.cinchapi.concourse.lang.Criteria;
 import com.cinchapi.concourse.lang.pagination.Page;
-import com.cinchapi.concourse.lang.sort.Order;
 import com.cinchapi.concourse.test.ConcourseIntegrationTest;
-import com.cinchapi.concourse.test.Variables;
 import com.cinchapi.concourse.thrift.Operator;
 import com.cinchapi.concourse.util.Resources;
-import com.google.common.collect.Sets;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.Set;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Unit tests to make sure that the select operation with {@link Page}
  * as a parameter works properly.
  */
 public class SelectWithPageTest extends ConcourseIntegrationTest {
-
-    private Statement sql;
 
     @Override
     protected void beforeEachTest() {
@@ -35,58 +44,32 @@ public class SelectWithPageTest extends ConcourseIntegrationTest {
         Importer importer = new CsvImporter(client);
         importer.importFile(Resources.get("/college.csv").getFile());
 
-        // Load up the SQL db which also contains a copy of the data
-        System.out.println("Loading SQL database with college data");
-        try {
-            // NOTE: The JDBC API is atrocious :o=
-            Class.forName("org.sqlite.JDBC");
-            Connection conn = DriverManager.getConnection(
-                    "jdbc:sqlite:" + Resources.get("/college.db").getFile());
-            sql = conn.createStatement();
-        }
-        catch (Exception e) {
-            throw CheckedExceptions.wrapAsRuntimeException(e);
-        }
-
         super.beforeEachTest();
     }
 
     @Test
     public void testWithPage() {
-        Assert.assertTrue(hasSameResults(Criteria.where().key("graduation_rate")
-                        .operator(Operator.GREATER_THAN).value(90).and()
-                        .key("yield_men").operator(Operator.EQUALS).value(20).build(),
-                Page.with(10).to(5)));
-    }
+        Criteria criteria = Criteria.where().key("graduation_rate")
+                .operator(Operator.GREATER_THAN).value(90).build();
+        Page page = Page.with(10).to(1);
 
-    /**
-     * Validate that the {@code criteria} and {@code page} returns the same
-     * result in Concourse as it does in a relational database.
-     *
-     * @param page
-     * @return {@code true} if the Concourse and SQL result sets are the same
-     */
-    private boolean hasSameResults(Criteria criteria, Page page) {
-        try {
-            Set<Object> a = Sets.newHashSet(
-                    client.get("ipeds_id", client.find(criteria)).values());
-            String query = "SELECT ipeds_id FROM data WHERE " + criteria.toString()
-                            + " LIMIT " + page.limit() + " OFFSET " + page.skip();
-            ResultSet rs = sql.executeQuery(query);
-            rs.next(); // skip column header
-            Set<Object> b = Sets.newHashSet();
-            while (rs.next()) {
-                b.add(rs.getInt(1));
-            }
-            Variables.register("query", query);
-            Variables.register("con", a);
-            Variables.register("sql", b);
-            Variables.register("diff", Sets.symmetricDifference(a, b));
-            return a.equals(b);
+        List<Map<String, Object>> result = client
+                .select(Lists.newArrayList("ipeds_id", "graduation_rate"),
+                        criteria, page)
+                .values().stream()
+                .map(child -> child.entrySet().stream()
+                        .collect(Collectors.toMap(Map.Entry::getKey,
+                                e -> Iterables.getOnlyElement(e.getValue()))))
+                .collect(Collectors.toList());
 
-        }
-        catch (Exception e) {
-            throw CheckedExceptions.wrapAsRuntimeException(e);
-        }
+        List<Map<String, Object>> expected = client
+                .select(Lists.newArrayList("ipeds_id", "graduation_rate"), criteria)
+                .values().stream()
+                .map(child -> child.entrySet().stream()
+                        .collect(Collectors.toMap(Map.Entry::getKey,
+                                e -> Iterables.getOnlyElement(e.getValue()))))
+                .collect(Collectors.toList());
+
+        Assert.assertEquals(expected.subList(0, 10), result);
     }
 }
