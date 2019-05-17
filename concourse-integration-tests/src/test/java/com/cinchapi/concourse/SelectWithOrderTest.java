ce17b63d19a1115b7058dbb71c21154c5b81c05f
javierlores <javier@cinchapi.com>
2019-05-17 13:57:48 -0400

    spotless apply

--- concourse-integration-tests/src/test/java/com/cinchapi/concourse/SelectWithOrderTest.java
+++ concourse-integration-tests/src/test/java/com/cinchapi/concourse/SelectWithOrderTest.java
@@ -15,57 +15,53 @@
  */
 package com.cinchapi.concourse;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.cinchapi.common.base.CheckedExceptions;
-import com.cinchapi.concourse.importer.CsvImporter;
-import com.cinchapi.concourse.importer.Importer;
 import com.cinchapi.concourse.lang.Criteria;
 import com.cinchapi.concourse.lang.sort.Order;
 import com.cinchapi.concourse.test.ConcourseIntegrationTest;
 import com.cinchapi.concourse.thrift.Operator;
-import com.cinchapi.concourse.util.Resources;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 
 /**
  * Unit tests to make sure that the select operation with
  * {@link Order} as a parameter work properly.
  */
 public class SelectWithOrderTest extends ConcourseIntegrationTest {
-    
+
     @Test
     public void testWithOneOrderKey() {
         Criteria criteria = Criteria.where().key("graduation_rate")
-                        .operator(Operator.GREATER_THAN).value(90).build();
+                .operator(Operator.GREATER_THAN).value(90).build();
         Order order = Order.by("graduation_rate").build();
 
-        List<Map<String, Object>> result =
-                client.select(Lists.newArrayList("ipeds_id", "graduation_rate"), criteria,
-                        order).values().stream().map(child ->
-                        child.entrySet().stream().
-                                collect(Collectors.toMap(Map.Entry::getKey,
-                                        e -> Iterables.getOnlyElement(e.getValue())))
-                ).collect(Collectors.toList());
+        List<Map<String, Object>> result = client
+                .select(Lists.newArrayList("ipeds_id", "graduation_rate"),
+                        criteria, order)
+                .values().stream()
+                .map(child -> child.entrySet().stream()
+                        .collect(Collectors.toMap(Map.Entry::getKey,
+                                e -> Iterables.getOnlyElement(e.getValue()))))
+                .collect(Collectors.toList());
 
-        List<Map<String, Object>> expected =
-                client.select(Lists.newArrayList("ipeds_id", "graduation_rate"), criteria)
-                        .values().stream().map(child ->
-                        child.entrySet().stream().
-                                collect(Collectors.toMap(Map.Entry::getKey,
-                                        e -> Iterables.getOnlyElement(e.getValue())))
-                ).collect(Collectors.toList());
+        List<Map<String, Object>> expected = client
+                .select(Lists.newArrayList("ipeds_id", "graduation_rate"),
+                        criteria)
+                .values().stream()
+                .map(child -> child.entrySet().stream()
+                        .collect(Collectors.toMap(Map.Entry::getKey,
+                                e -> Iterables.getOnlyElement(e.getValue()))))
+                .collect(Collectors.toList());
 
-        expected.sort(Comparator.comparingInt(o -> (int) o.get("graduation_rate")));
+        expected.sort(
+                Comparator.comparingInt(o -> (int) o.get("graduation_rate")));
 
         Assert.assertEquals(result, expected);
     }
@@ -74,33 +70,36 @@ public class SelectWithOrderTest extends ConcourseIntegrationTest {
     public void testWithTwoOrderKeys() {
         Criteria criteria = Criteria.where().key("graduation_rate")
                 .operator(Operator.GREATER_THAN).value(90).build();
-        Order order = Order.by("graduation_rate").ascending()
-                .then("city").ascending()
-                .build();
+        Order order = Order.by("graduation_rate").ascending().then("city")
+                .ascending().build();
 
-        List<Map<String, Object>> result =
-                client.select(Lists.newArrayList("ipeds_id", "graduation_rate", "city"), criteria,
-                        order).values().stream().map(child ->
-                            child.entrySet().stream().
-                                    collect(Collectors.toMap(Map.Entry::getKey,
-                                            e -> Iterables.getOnlyElement(e.getValue())))
-                ).collect(Collectors.toList());
+        List<Map<String, Object>> result = client
+                .select(Lists.newArrayList("ipeds_id", "graduation_rate",
+                        "city"), criteria, order)
+                .values().stream()
+                .map(child -> child.entrySet().stream()
+                        .collect(Collectors.toMap(Map.Entry::getKey,
+                                e -> Iterables.getOnlyElement(e.getValue()))))
+                .collect(Collectors.toList());
 
-        List<Map<String, Object>> expected =
-                client.select(Lists.newArrayList("ipeds_id", "graduation_rate", "city"), criteria)
-                        .values().stream().map(child ->
-                        child.entrySet().stream().
-                                collect(Collectors.toMap(Map.Entry::getKey,
-                                        e -> Iterables.getOnlyElement(e.getValue())))
-                ).collect(Collectors.toList());
+        List<Map<String, Object>> expected = client
+                .select(Lists.newArrayList("ipeds_id", "graduation_rate",
+                        "city"), criteria)
+                .values().stream()
+                .map(child -> child.entrySet().stream()
+                        .collect(Collectors.toMap(Map.Entry::getKey,
+                                e -> Iterables.getOnlyElement(e.getValue()))))
+                .collect(Collectors.toList());
 
         expected.sort((o1, o2) -> {
-                int c;
-                c = ((Integer) o1.get("graduation_rate")).compareTo((Integer) o2.get("graduation_rate"));
-                if (c == 0)
-                    c = ((String) o1.get("city")).compareTo((String) o2.get("city"));
-                return c;
-            });
+            int c;
+            c = ((Integer) o1.get("graduation_rate"))
+                    .compareTo((Integer) o2.get("graduation_rate"));
+            if(c == 0)
+                c = ((String) o1.get("city"))
+                        .compareTo((String) o2.get("city"));
+            return c;
+        });
 
         Assert.assertEquals(result, expected);
     }
