acee59397a9ab0622e9f6740c68752377de9c987
javierlores <javier@cinchapi.com>
2019-06-24 11:42:18 -0400

    Removed some selection keys tests

--- concourse-integration-tests/src/test/java/com/cinchapi/concourse/NavigateKeysTest.java
+++ concourse-integration-tests/src/test/java/com/cinchapi/concourse/NavigateKeysTest.java
@@ -58,56 +58,6 @@ public class NavigateKeysTest extends ConcourseIntegrationTest {
         return expected;
     }
 
-    /**
-     * Setup data for each of the tests.
-     *
-     * @param client
-     */
-    private static Map<Long, Map<String, Set<Object>>> setupNavigateSelectionAndEvaluation(
-            Concourse client) {
-        client.add("name", "john", 1);
-        client.add("mother", Link.to(2), 1);
-
-        client.add("name", "leslie", 2);
-        client.add("children", 3, 2);
-
-        client.add("name", "doe", 3);
-        client.add("mother", Link.to(4), 3);
-
-        client.add("name", "martha", 4);
-        client.add("children", 2, 4);
-
-        Map<Long, Map<String, Set<Object>>> expected = Maps.newHashMap();
-        Map<String, Set<Object>> row1 = Maps.newHashMap();
-        expected.put(1L, row1);
-        row1.put("name", Sets.newHashSet("john"));
-        row1.put("mother.children", Sets.newHashSet(3));
-
-        return expected;
-    }
-
-    /**
-     * Setup data for each of the tests.
-     *
-     * @param client
-     */
-    private static Set<Long> setupNavigateFindSelection(
-            Concourse client) {
-        client.add("name", "john", 1);
-        client.add("mother", Link.to(2), 1);
-
-        client.add("name", "leslie", 2);
-        client.add("children", 3, 2);
-
-        client.add("name", "doe", 3);
-        client.add("mother", Link.to(4), 3);
-
-        client.add("name", "martha", 4);
-        client.add("children", 2, 4);
-
-        return Sets.newHashSet(3L);
-    }
-
     @Test
     public void testNavigateAsEvaluationKey() {
         Map<Long, Map<String, Set<Object>>> expected = setupNavigateEvaluation(
