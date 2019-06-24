a15fe7b4042885113d6eb8333361660fe2c83a15
javierlores <javier@cinchapi.com>
2019-06-24 12:07:07 -0400

    Added tests

--- concourse-integration-tests/src/test/java/com/cinchapi/concourse/NavigateKeysTest.java
+++ concourse-integration-tests/src/test/java/com/cinchapi/concourse/NavigateKeysTest.java
@@ -18,6 +18,8 @@ package com.cinchapi.concourse;
 import java.util.Map;
 import java.util.Set;
 
+import com.cinchapi.concourse.thrift.Operator;
+import com.google.common.collect.Lists;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -57,12 +59,79 @@ public class NavigateKeysTest extends ConcourseIntegrationTest {
         return expected;
     }
 
+    /**
+     * Setup data for each of the tests.
+     *
+     * @param client
+     */
+    private static Set<Long> setupNavigateFindEvaluation(
+            Concourse client) {
+        client.add("name", "john", 1);
+        client.add("mother", Link.to(2), 1);
+
+        client.add("name", "leslie", 2);
+        client.add("children", 3, 2);
+
+        client.add("name", "doe", 3);
+        client.add("mother", Link.to(4), 3);
+
+        client.add("name", "martha", 4);
+        client.add("children", 2, 4);
+
+        return Sets.newHashSet(3L);
+    }
+
+    /**
+     * Setup data for each of the tests.
+     *
+     * @param client
+     */
+    private static Map<Long, Map<String, Set<Object>>> setupNavigateSelectionEvaluation(
+            Concourse client) {
+        client.add("name", "john", 1);
+        client.add("mother", Link.to(2), 1);
+
+        client.add("name", "leslie", 2);
+        client.add("children", 3, 2);
+
+        client.add("name", "doe", 3);
+        client.add("mother", Link.to(4), 3);
+
+        client.add("name", "martha", 4);
+        client.add("children", 2, 4);
+
+        Map<Long, Map<String, Set<Object>>> expected = Maps.newHashMap();
+        Map<String, Set<Object>> row1 = Maps.newHashMap();
+        expected.put(1L, row1);
+        row1.put("name", Sets.newHashSet("john"));
+        row1.put("mother.children", Sets.newHashSet(3));
+
+        return expected;
+    }
+
     @Test
     public void testNavigateAsEvaluationKey() {
         Map<Long, Map<String, Set<Object>>> expected = setupNavigateEvaluation(
                 client);
         Map<Long, Map<String, Set<Object>>> actual = client
                 .select("mother.children = 3");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNavigateFindAsEvaluationKey() {
+        Set<Long> expected = setupNavigateFindEvaluation(client);
+        Set<Long> actual = client
+                .find("mother.children", Operator.EQUALS, 2);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNavigateAsSelectionAndEvaluationKey() {
+        Map<Long, Map<String, Set<Object>>> expected = setupNavigateSelectionEvaluation(
+                client);
+        Map<Long, Map<String, Set<Object>>> actual = client
+                .select(Lists.newArrayList("name", "mother.children"), "mother.children = 3");
         Assert.assertEquals(expected, actual);
     }
 }
