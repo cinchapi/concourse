485ba8f0fda0045e309425305accc0d6aaca1f7a
Jeff Nelson <jeff@cinchapi.com>
2019-06-29 15:07:54 -0400

    refactor tests slightly

--- concourse-ete-tests/src/test/java/com/cinchapi/concourse/ete/bugrepro/CON649.java
+++ concourse-ete-tests/src/test/java/com/cinchapi/concourse/ete/bugrepro/CON649.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.cinchapi.concourse.bugrepro;
+package com.cinchapi.concourse.ete.bugrepro;
 
 import java.io.IOException;
 import java.nio.file.Files;
--- concourse-ete-tests/src/test/java/com/cinchapi/concourse/ete/performance/Version0_10PerformanceTest.java
+++ concourse-ete-tests/src/test/java/com/cinchapi/concourse/ete/performance/Version0_10PerformanceTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.cinchapi.concourse.performance;
+package com.cinchapi.concourse.ete.performance;
 
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
