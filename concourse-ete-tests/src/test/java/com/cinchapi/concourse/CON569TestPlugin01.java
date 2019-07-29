09521a2b3b4f27ea0586be830759b27970892445
asyncRobert <nicksums23@gmail.com>
2019-07-29 10:00:59 -0400

    CON569TestPlugin01, InspectTest

--- concourse-ete-tests/src/test/java/com/cinchapi/concourse/CON569TestPlugin01.java
+++ concourse-ete-tests/src/test/java/com/cinchapi/concourse/CON569TestPlugin01.java
@@ -1,8 +1,8 @@
 package com.cinchapi.concourse;
 
-import com.cinchapi.concourse.server.plugin.TestPlugin;
+import com.cinchapi.concourse.server.plugin.Plugin;
 
-public class CON569TestPlugin01 extends TestPlugin {
+public class CON569TestPlugin01 extends Plugin {
 
     public CON569TestPlugin01(String fromServer, String fromPlugin) {
         super(fromServer, fromPlugin);
--- concourse-ete-tests/src/test/java/com/cinchapi/concourse/InspectTest.java
+++ concourse-ete-tests/src/test/java/com/cinchapi/concourse/InspectTest.java
@@ -1,9 +1,17 @@
 package com.cinchapi.concourse;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Map;
+import java.util.Set;
+
 import org.junit.Test;
 
 import com.cinchapi.concourse.test.ClientServerTest;
 import com.cinchapi.concourse.test.PluginTest;
+import com.google.common.collect.Maps;
+
+import org.junit.Assert;
 
 public class InspectTest extends ClientServerTest implements PluginTest {
 
@@ -14,16 +22,29 @@ public class InspectTest extends ClientServerTest implements PluginTest {
     
     @Test
     public void testInspect() {
+        final String TEST_PLUGIN_NAME = "com.cinchapi.concourse.CON569TestPlugin01";
         
-        //know expected results
+        Map<String, Set<String>> descriptions = Maps.newHashMap();
         
-//        client.inspect();
+        Method[] methods = CON569TestPlugin01.class.getMethods();
         
-       //assert if printed results were expected.
+        for (Method method : methods) {
+            Parameter[] parameters = method.getParameters();
+            StringBuilder builder = new StringBuilder(
+                    method.getName());
+            builder.append("(");
+            for (Parameter parameter : parameters) {
+                builder.append(parameter.getType().getName()).append(" ");
+            }
+            builder.append(")");
+            descriptions.get(CON569TestPlugin01.class.getName()).add(builder.toString());
+        }
+        
+        Assert.assertTrue("Descriptions contains no available plugins.", descriptions.isEmpty());
+        Assert.assertTrue("Descriptions does not contain the test plugin.", descriptions.containsKey(TEST_PLUGIN_NAME));
+        Assert.assertTrue("CON569TestPlugin01 does not contain say(java.lang.String)", descriptions.get(TEST_PLUGIN_NAME).contains("say(java.lang.String )"));
+        Assert.assertTrue("CON569TestPlugin01 does not contain bye()", descriptions.get(TEST_PLUGIN_NAME).contains("bye()"));
     }
-
-
-    
 
 }
 
