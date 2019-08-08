5c445f93bdbd9496d072d25cb01ee35ff0bf25e5
Tyler Brown <tyler@sliceofnet.com>
2019-08-07 20:51:10 -0400

    spotlessApply

--- concourse-export/src/main/java/com.cinchapi.concourse.exporter/ExportCli.java
+++ concourse-export/src/main/java/com.cinchapi.concourse.exporter/ExportCli.java
@@ -1,8 +1,19 @@
+/*
+ * Copyright (c) 2013-2019 Cinchapi Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.cinchapi.concourse.exporter;
-
-import com.cinchapi.common.base.AnyStrings;
-import com.cinchapi.concourse.cli.CommandLineInterface;
-import jline.console.ConsoleReader;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -15,27 +26,33 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import jline.console.ConsoleReader;
+
+import com.cinchapi.common.base.AnyStrings;
+import com.cinchapi.concourse.cli.CommandLineInterface;
+
 public class ExportCli extends CommandLineInterface<ExportOptions> {
     @Override
     protected void doTask() {
-        final Set<Long> recordIDs = options.records.size() > 0
-                ? options.records
+        final Set<Long> recordIDs = options.records.size() > 0 ? options.records
                 : concourse.inventory();
 
         Path path = createFile();
 
         final Iterable<Map<String, Object>> records = (options.criteria != null
                 ? concourse.get(options.criteria).entrySet().stream()
-                    .filter(r -> recordIDs.contains(r.getKey()))
-                    .map(Map.Entry::getValue)
+                        .filter(r -> recordIDs.contains(r.getKey()))
+                        .map(Map.Entry::getValue)
                 : concourse.describe(recordIDs).entrySet().stream()
-                    .map(e -> concourse.get(e.getValue(), e.getKey()))
-                ).collect(Collectors.toList());
+                        .map(e -> concourse.get(e.getValue(), e.getKey())))
+                                .collect(Collectors.toList());
 
         try {
             output(records, Files.newOutputStream(path));
-        } catch(IOException exception) {
-            throw new RuntimeException("Failed to create a stream for the file.");
+        }
+        catch (IOException exception) {
+            throw new RuntimeException(
+                    "Failed to create a stream for the file.");
         }
     }
 
@@ -47,14 +64,16 @@ public class ExportCli extends CommandLineInterface<ExportOptions> {
     private Path createFile() {
         try {
             ConsoleReader reader = new ConsoleReader();
-            System.out.println("What's the path of the file you want to create?");
+            System.out
+                    .println("What's the path of the file you want to create?");
             Path path = getPathOrNull(reader.readLine());
 
             if(path == null) {
                 System.out.println("Please enter a valid path.");
             }
             return path == null ? createFile() : Files.createFile(path);
-        } catch(IOException e) {
+        }
+        catch (IOException e) {
             throw new RuntimeException("Failed to interact with stdin.");
         }
     }
@@ -62,20 +81,21 @@ public class ExportCli extends CommandLineInterface<ExportOptions> {
     private Path getPathOrNull(String path) {
         try {
             return Paths.get(path);
-        } catch (InvalidPathException | NullPointerException e) {
+        }
+        catch (InvalidPathException | NullPointerException e) {
             return null;
         }
     }
 
     // TODO: Replace the functions below this line with the library function
     // TODO: When https://github.com/cinchapi/accent4j/pull/10 is merged
-    
+
     private static <T, Q> void output(Iterable<Map<T, Q>> items,
             OutputStream output) {
         PrintStream printer = new PrintStream(output);
 
-        for(Map<T, Q> item : items) {
-            for(Map.Entry<T, Q> entry : item.entrySet()) {
+        for (Map<T, Q> item : items) {
+            for (Map.Entry<T, Q> entry : item.entrySet()) {
                 outputItem(printer, entry.getKey(), entry.getValue());
             }
         }
@@ -83,8 +103,8 @@ public class ExportCli extends CommandLineInterface<ExportOptions> {
 
     private static <T, Q> void outputItem(PrintStream printer, T k, Q v) {
         if(v instanceof String) {
-            printer.println(k + "," + AnyStrings
-                    .ensureWithinQuotesIfNeeded((String) v, ','));
+            printer.println(k + ","
+                    + AnyStrings.ensureWithinQuotesIfNeeded((String) v, ','));
         }
         printer.println(k + "," + v);
     }
--- concourse-export/src/main/java/com.cinchapi.concourse.exporter/ExportOptions.java
+++ concourse-export/src/main/java/com.cinchapi.concourse.exporter/ExportOptions.java
@@ -1,19 +1,35 @@
+/*
+ * Copyright (c) 2013-2019 Cinchapi Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.cinchapi.concourse.exporter;
-
-import com.beust.jcommander.Parameter;
-import com.cinchapi.concourse.cli.Options;
 
 import java.util.HashSet;
 import java.util.Set;
 
+import com.beust.jcommander.Parameter;
+import com.cinchapi.concourse.cli.Options;
+
 public class ExportOptions extends Options {
-    @Parameter(names = { "-r",  "--records" }, description = "Comma separated list of records to export")
+    @Parameter(names = { "-r",
+            "--records" }, description = "Comma separated list of records to export")
     public Set<Long> records = new HashSet<>();
 
-    @Parameter(names = { "-c", "-ccl" },
-            description = "Criteria statement (in CCL) that describes how to find the records to export")
+    @Parameter(names = { "-c",
+            "-ccl" }, description = "Criteria statement (in CCL) that describes how to find the records to export")
     public String criteria = null;
 
-    @Parameter(names = "--no-primary-key", description="Flag to not display the primary key when exporting.")
+    @Parameter(names = "--no-primary-key", description = "Flag to not display the primary key when exporting.")
     public boolean hidePrimaryKey = false;
 }
