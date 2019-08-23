65371f30755552d52d4225f174feceac016c0fbd
Jeff Nelson <jeff@jeffnelson.com>
2019-08-23 07:01:31 -0400

    swap out manual index queue with the full power of the ExecutorService's queueing

--- concourse-server/src/main/java/com/cinchapi/concourse/server/storage/db/search/SearchIndexer.java
+++ concourse-server/src/main/java/com/cinchapi/concourse/server/storage/db/search/SearchIndexer.java
@@ -15,18 +15,13 @@
  */
 package com.cinchapi.concourse.server.storage.db.search;
 
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import javax.annotation.concurrent.Immutable;
-
-import com.cinchapi.common.base.CheckedExceptions;
 import com.cinchapi.common.concurrent.CountUpLatch;
 import com.cinchapi.concourse.server.model.Position;
 import com.cinchapi.concourse.server.model.Text;
 import com.cinchapi.concourse.server.storage.Action;
-import com.google.common.collect.Queues;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 /**
@@ -61,11 +56,6 @@ public final class SearchIndexer {
     }
 
     /**
-     * The queue where insertion requests are held.
-     */
-    private final BlockingQueue<Entry> queue;
-
-    /**
      * An {@link ExecutorService} that controls the worker threads.
      */
     private final ExecutorService workers;
@@ -76,29 +66,9 @@ public final class SearchIndexer {
      * @param numWorkers
      */
     private SearchIndexer(int numWorkers) {
-        this.queue = Queues.newLinkedBlockingQueue();
         this.workers = Executors.newFixedThreadPool(numWorkers,
                 new ThreadFactoryBuilder().setDaemon(true)
                         .setNameFormat("Search Indexer" + " %d").build());
-        for (int i = 0; i < numWorkers; ++i) {
-            workers.execute(() -> {
-                for (;;) {
-                    // Each worker repeatedly grabs an Entry from the #queue
-                    // (blocking if necessary) and inserts the appropriate data
-                    // into the Entry's SearchIndex.
-                    try {
-                        Entry entry = queue.take();
-                        SearchIndex index = entry.index;
-                        index.index(entry.key, Text.wrapCached(entry.term),
-                                entry.position, entry.version, entry.type);
-                        entry.ticker.countUp();
-                    }
-                    catch (InterruptedException e) {
-                        throw CheckedExceptions.wrapAsRuntimeException(e);
-                    }
-                }
-            });
-        }
     }
 
     /**
@@ -116,80 +86,10 @@ public final class SearchIndexer {
      */
     public void enqueue(SearchIndex index, CountUpLatch ticker, Text key,
             String term, Position position, long version, Action type) {
-        Entry entry = new Entry(index, ticker, key, term, position, version,
-                type);
-        queue.add(entry);
-    }
-
-    /**
-     * Entry held in the {@link #queue}.
-     *
-     * @author Jeff Nelson
-     */
-    @Immutable
-    private class Entry {
-
-        /**
-         * The key (e.g. the corresponding SearchBlock's locator) for which the
-         * {@link #term} at {@link #position} is searchable.
-         */
-        private final Text key;
-
-        /**
-         * The search term.
-         */
-        private final String term;
-
-        /**
-         * The {@link Position} of the search term in the data universe.
-         */
-        private final Position position;
-
-        /**
-         * The version of the data that produced this {@link Entry}.
-         */
-        private final long version;
-
-        /**
-         * The {@link Action} that produced this data.
-         */
-        private final Action type;
-
-        /**
-         * A {@link CountUpLatch} that is incremented when this {@link Entry} is
-         * {@link SearchIndex#index(Text, Text, Position, long, Action)
-         * indexed}.
-         */
-        private final CountUpLatch ticker;
-
-        /**
-         * The {@link SearchIndex} where this {@link Entry} will be
-         * {@link SearchIndex#index(Text, Text, Position, long, Action) indexed}
-         * and stored.
-         */
-        private final SearchIndex index;
-
-        /**
-         * Construct a new instance.
-         * 
-         * @param index
-         * @param ticker
-         * @param key
-         * @param term
-         * @param position
-         * @param version
-         * @param type
-         */
-        Entry(SearchIndex index, CountUpLatch ticker, Text key, String term,
-                Position position, long version, Action type) {
-            this.index = index;
-            this.ticker = ticker;
-            this.key = key;
-            this.term = term;
-            this.position = position;
-            this.version = version;
-            this.type = type;
-        }
+        workers.execute(() -> {
+            index.index(key, Text.wrapCached(term), position, version, type);
+            ticker.countUp();
+        });
     }
 
 }
