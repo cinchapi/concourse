24208f82dff5dc15fce48be346c6a8fbb9b10f64
Jeff Nelson <jeff@cinchapi.com>
2020-01-01 16:40:47 -0500

    more strategy tweaks

--- concourse-server/src/main/java/com/cinchapi/concourse/server/ops/Strategy.java
+++ concourse-server/src/main/java/com/cinchapi/concourse/server/ops/Strategy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2019 Cinchapi Inc.
+ * Copyright (c) 2013-2020 Cinchapi Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,6 +39,11 @@ public class Strategy {
     private final Store store;
 
     /**
+     * A boolean that tracks if the {@link #store} is {@link Gatherable}.
+     */
+    private final boolean gatherable;
+
+    /**
      * Construct a new instance.
      * 
      * @param request
@@ -47,6 +52,7 @@ public class Strategy {
     public Strategy(Request request, Store store) {
         this.request = request;
         this.store = store;
+        this.gatherable = store instanceof Gatherable;
     }
 
     /**
@@ -60,43 +66,32 @@ public class Strategy {
      */
     public Source source(String key, long record) {
         Memory memory = store.memory();
+        boolean isWideOperation = request.operationKeys().isEmpty()
+                && !request.operation().startsWith("find");
+        boolean isConditionKey = request.conditionKeys().contains(key);
+        boolean isOperationKey = isWideOperation
+                || request.operationKeys().contains(key);
+        boolean isOrderKey = request.orderKeys().contains(key);
         Source source;
-        if(memory.contains(key, record)) {
-            source = Source.FIELD;
-        }
-        else if(request.operationKeys().isEmpty()
-                && !request.operation().startsWith("find")) {
+        if(isWideOperation) {
             // The entire record is involved in the operation, so force the full
             // PrimaryRecord to be loaded.
             source = Source.RECORD;
         }
-        else if(request.operationKeys().isEmpty()
-                && request.operationRecords().isEmpty()
-                && request.operationKeys().size() >= request.operationRecords()
-                        .size()) {
-            // TODO: calibrate...may want the Field for this...
-            source = Source.RECORD;
-        }
-        else if(!(store instanceof Gatherable)) {
-            source = memory.contains(record) ? Source.RECORD : Source.FIELD;
-        }
-        // NOTE: the following conditions can only occur for a Gatherable store
-        else if(memory.contains(key)) {
+        else if(request.conditionKeys().contains(key)
+                && request.operationRecords().size() != 1 && gatherable) {
+            // The SecondaryRecord must be loaded to evaluate the condition, so
+            // leverage it to gather the values for key/record
             source = Source.INDEX;
-        }
-        else if(request.conditionKeys().contains(key)) {
-            source = Source.INDEX;
-        }
-        else if(request.operationKeys().size() < request.operationRecords()
-                .size()) { // TODO: calibrate?
-            source = Source.INDEX;
-        }
-        else if(memory.contains(record)) {
-            source = Source.RECORD;
         }
         else {
             source = Source.FIELD;
         }
+
+        if(source == Source.INDEX && !gatherable) {
+            source = memory.contains(record) ? Source.RECORD : Source.FIELD;
+        }
+
         return source;
     }
 
