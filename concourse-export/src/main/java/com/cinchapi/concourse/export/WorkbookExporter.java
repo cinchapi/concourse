b234eb04a4e3315e03426f8633aabdc662643155
Jeff Nelson <jeff@cinchapi.com>
2020-03-02 11:26:57 -0500

    fix formatting

--- concourse-export/src/main/java/com/cinchapi/concourse/export/WorkbookExporter.java
+++ concourse-export/src/main/java/com/cinchapi/concourse/export/WorkbookExporter.java
@@ -1,28 +1,17 @@
 /*
- * Cinchapi Inc. CONFIDENTIAL
- * Copyright (c) 2020 Cinchapi Inc. All Rights Reserved.
+ * Copyright (c) 2013-2019 Cinchapi Inc.
  *
- * All information contained herein is, and remains the property of Cinchapi.
- * The intellectual and technical concepts contained herein are proprietary to
- * Cinchapi and may be covered by U.S. and Foreign Patents, patents in process,
- * and are protected by trade secret or copyright law. Dissemination of this
- * information or reproduction of this material is strictly forbidden unless
- * prior written permission is obtained from Cinchapi. Access to the source code
- * contained herein is hereby forbidden to anyone except current Cinchapi
- * employees, managers or contractors who have executed Confidentiality and
- * Non-disclosure agreements explicitly covering such access.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * The copyright notice above does not evidence any actual or intended
- * publication or disclosure of this source code, which includes information
- * that is confidential and/or proprietary, and is a trade secret, of Cinchapi.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC PERFORMANCE, OR PUBLIC
- * DISPLAY OF OR THROUGH USE OF THIS SOURCE CODE WITHOUT THE EXPRESS WRITTEN
- * CONSENT OF COMPANY IS STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE
- * LAWS AND INTERNATIONAL TREATIES. THE RECEIPT OR POSSESSION OF THIS SOURCE
- * CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS TO
- * REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR
- * SELL ANYTHING THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.cinchapi.concourse.export;
 
