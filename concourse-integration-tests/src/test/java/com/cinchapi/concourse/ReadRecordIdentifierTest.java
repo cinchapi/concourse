8b68b2944165cfb1762788c96beb57599fdd6911
Jeff Nelson <jeff@cinchapi.com>
2022-03-19 21:50:12 -0400

    Added support for querying on Record ids (#474)
    * Added support for querying on the intrinsic `identifier` of Records, as both a selection and evaluation key. The record identifier can be refernced using the `$id$` key (NOTE: this must be properly escaped in `concourse shell` as `\$id\$`).
    	* It is useful to include the Record identifier as a selection key for some navigation reads (e.g., `select(["partner.name", parner.\$id\$], 1)`)).
    	* It is useful to include the Record identifier as an evaluation key in cases where you want to explictly exclude a record from matching a `Condition` (e.g., `select(["partner.name", parner.\$id\$], "\$id\$ != 2")`))
    
    * add unit tests

---
+++ concourse-integration-tests/src/test/java/com/cinchapi/concourse/ReadRecordIdentifierTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2013-2022 Cinchapi Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cinchapi.concourse;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.cinchapi.concourse.test.ConcourseIntegrationTest;
+import com.google.common.collect.ImmutableSet;
+
+/**
+ * Unit tests for querying on record identifiers.
+ *
+ * @author Jeff Nelson
+ */
+public class ReadRecordIdentifierTest extends ConcourseIntegrationTest {
+
+    private final String key = Constants.JSON_RESERVED_IDENTIFIER_NAME;
+
+    @Override
+    public void beforeEachTest() {
+        client.add("name", "Jeff Nelson", 1);
+        client.add("name", "Ashleah Nelson", 2);
+        client.add("name", "Reginald Moore", 3);
+        client.add("name", "Aevyn Nelson", 4);
+    }
+
+    @Test
+    public void testSelectionKey() {
+        Set<Long> data = client.select(key, 1);
+        Assert.assertEquals(ImmutableSet.of(1L), data);
+    }
+
+    @Test
+    public void testSelectionKeyCcl() {
+        Map<Long, Set<Long>> data = client.select(key, "name like %Nelson%");
+        data.forEach((key, values) -> {
+            Assert.assertEquals(ImmutableSet.of(key), values);
+        });
+    }
+
+    @Test
+    public void testNavigationSelectionKey() {
+        client.link("spouse", 2, 1);
+        client.link("spouse", 1, 2);
+        Map<String, Set<Object>> data = client.select(
+                ImmutableSet.of("name", "spouse.name", "spouse.$id$"), 1);
+        Assert.assertEquals(ImmutableSet.of(2L), data.get("spouse.$id$"));
+    }
+
+    @Test
+    public void testEvaluationKey() {
+        Set<Long> data = client.find("name like %Nelson% and $id$ != 1");
+        Assert.assertFalse(data.contains(1L));
+    }
+
+    @Test
+    @Ignore // requires CCL bug fix
+    public void testNavigationEvaluationKey() {
+        Set<Long> data = client.find("name like %Nelson% and spouse.$id$ != 1");
+        Assert.assertFalse(data.contains(2L));
+    }
+
+}
