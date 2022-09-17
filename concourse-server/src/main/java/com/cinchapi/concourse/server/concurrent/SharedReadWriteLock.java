a68a0f3ff8e6e23e383cc694b882ca6ef766dd00
Jeff Nelson <jeff@jeffnelson.com>
2022-09-17 18:36:33 -0400

    add SharedReadWriteLock

---
+++ concourse-server/src/main/java/com/cinchapi/concourse/server/concurrent/SharedReadWriteLock.java
@@ -0,0 +1,254 @@
+/*
+ * Copyright (c) 2013-2022 Cinchapi Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cinchapi.concourse.server.concurrent;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.StampedLock;
+
+import com.google.common.base.Stopwatch;
+
+/**
+ * A {@link ReadWriteLock} that permits either multiple concurrent readers
+ * OR multiple concurrent writers, at a time.
+ *
+ * @author Jeff Nelson
+ */
+class SharedReadWriteLock implements ReadWriteLock {
+
+    /**
+     * An internal lock that controls concurrent access; allowing multiple
+     * readers and blocking writers.
+     */
+    private final ReadWriteLock readers;
+
+    /**
+     * An internal lock that controls concurrent access; allowing multiple
+     * writers and blocking readers.
+     */
+    private final ReadWriteLock writers;
+
+    /**
+     * Construct a new instance.
+     */
+    SharedReadWriteLock() {
+        this.readers = new StampedLock().asReadWriteLock();
+        this.writers = new StampedLock().asReadWriteLock();
+    }
+
+    @Override
+    public Lock readLock() {
+        return new SharedReadLock();
+    }
+
+    @Override
+    public Lock writeLock() {
+        return new SharedWriteLock();
+    }
+
+    /**
+     * Read view of this {@link SharedReadWriteLock}.
+     *
+     * @author Jeff Nelson
+     */
+    class SharedReadLock implements Lock {
+
+        @Override
+        public void lock() {
+            boolean locked = false;
+            while (!locked) {
+                writers.writeLock().lock();
+                locked = readers.readLock().tryLock();
+                writers.writeLock().unlock();
+                if(!locked) {
+                    Thread.yield();
+                }
+            }
+        }
+
+        @Override
+        public void lockInterruptibly() throws InterruptedException {
+            boolean locked = false;
+            while (!locked) {
+                writers.writeLock().lockInterruptibly();
+                locked = readers.readLock().tryLock();
+                writers.writeLock().unlock();
+                if(!locked) {
+                    Thread.yield();
+                }
+            }
+        }
+
+        @Override
+        public Condition newCondition() {
+            return readers.readLock().newCondition();
+        }
+
+        @Override
+        public boolean tryLock() {
+            if(writers.writeLock().tryLock()) {
+                for (;;) {
+                    try {
+                        if(readers.readLock().tryLock()) {
+                            return true;
+                        }
+                    }
+                    finally {
+                        writers.writeLock().unlock();
+                    }
+                    Thread.yield();
+                }
+            }
+            else {
+                return false;
+            }
+        }
+
+        @Override
+        public boolean tryLock(long time, TimeUnit unit)
+                throws InterruptedException {
+            Stopwatch watch = Stopwatch.createStarted();
+            if(writers.writeLock().tryLock(time, unit)) {
+                for (;;) {
+                    watch.stop();
+                    long elapsed = watch.elapsed(unit);
+                    time = time - elapsed;
+                    watch.start();
+                    try {
+                        if(readers.readLock().tryLock(time, unit)) {
+                            return true;
+                        }
+                    }
+                    finally {
+                        writers.writeLock().unlock();
+                    }
+                    watch.stop();
+                    elapsed = watch.elapsed(unit);
+                    time = time - elapsed;
+                    watch.start();
+                    Thread.yield();
+                }
+            }
+            else {
+                return false;
+            }
+        }
+
+        @Override
+        public void unlock() {
+            readers.readLock().unlock();
+        }
+
+    }
+
+    /**
+     * Write view of this {@link SharedReadWriteLock}.
+     *
+     * @author Jeff Nelson
+     */
+    class SharedWriteLock implements Lock {
+
+        @Override
+        public void lock() {
+            boolean locked = false;
+            while (!locked) {
+                readers.writeLock().lock();
+                locked = writers.readLock().tryLock();
+                readers.writeLock().unlock();
+                if(!locked) {
+                    Thread.yield();
+                }
+            }
+        }
+
+        @Override
+        public void lockInterruptibly() throws InterruptedException {
+            boolean locked = false;
+            while (!locked) {
+                readers.writeLock().lockInterruptibly();
+                locked = writers.readLock().tryLock();
+                readers.writeLock().unlock();
+                if(!locked) {
+                    Thread.yield();
+                }
+            }
+        }
+
+        @Override
+        public Condition newCondition() {
+            return writers.readLock().newCondition();
+        }
+
+        @Override
+        public boolean tryLock() {
+            if(readers.writeLock().tryLock()) {
+                for (;;) {
+                    try {
+                        if(writers.readLock().tryLock()) {
+                            return true;
+                        }
+                    }
+                    finally {
+                        readers.writeLock().unlock();
+                    }
+                    Thread.yield();
+                }
+            }
+            else {
+                return false;
+            }
+        }
+
+        @Override
+        public boolean tryLock(long time, TimeUnit unit)
+                throws InterruptedException {
+            Stopwatch watch = Stopwatch.createStarted();
+            if(readers.writeLock().tryLock(time, unit)) {
+                for (;;) {
+                    watch.stop();
+                    long elapsed = watch.elapsed(unit);
+                    time = time - elapsed;
+                    watch.start();
+                    try {
+                        if(writers.readLock().tryLock(time, unit)) {
+                            return true;
+                        }
+                    }
+                    finally {
+                        readers.writeLock().unlock();
+                    }
+                    watch.stop();
+                    elapsed = watch.elapsed(unit);
+                    time = time - elapsed;
+                    watch.start();
+                    Thread.yield();
+                }
+            }
+            else {
+                return false;
+            }
+        }
+
+        @Override
+        public void unlock() {
+            writers.readLock().unlock();
+        }
+
+    }
+
+}
---
+++ concourse-server/src/test/java/com/cinchapi/concourse/server/concurrent/SharedReadWriteLockTest.java
@@ -0,0 +1,240 @@
+/*
+ * Copyright (c) 2013-2022 Cinchapi Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cinchapi.concourse.server.concurrent;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.cinchapi.concourse.test.ConcourseBaseTest;
+import com.cinchapi.concourse.time.Time;
+import com.cinchapi.concourse.util.TestData;
+import com.google.common.collect.Lists;
+
+/**
+ *
+ *
+ * @author jeff
+ */
+public class SharedReadWriteLockTest extends ConcourseBaseTest {
+    
+    private SharedReadWriteLock lock;
+    
+    @Override
+    public void beforeEachTest() {
+        lock = new SharedReadWriteLock();
+    }
+    
+    @Test
+    public void testMultipleConcurrentWriters() {
+        List<Thread> threads = Lists.newArrayList();
+        final AtomicBoolean success = new AtomicBoolean(true);
+        Runnable runnable = new Runnable() {
+
+            @Override
+            public void run() {
+                if(!lock.writeLock().tryLock()) {
+                    success.set(false);
+                }
+            }
+
+        };
+        for (int i = 0; i < TestData.getScaleCount(); i++) {
+            threads.add(new Thread(runnable));
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        Assert.assertTrue(success.get());
+    }
+    
+    @Test
+    public void testMultipleConcurrentReaders() {
+        List<Thread> threads = Lists.newArrayList();
+        final AtomicBoolean success = new AtomicBoolean(true);
+        Runnable runnable = new Runnable() {
+
+            @Override
+            public void run() {
+                if(!lock.readLock().tryLock()) {
+                    success.set(false);
+                }
+            }
+
+        };
+        for (int i = 0; i < TestData.getScaleCount(); i++) {
+            threads.add(new Thread(runnable));
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        Assert.assertTrue(success.get());
+    }
+    
+    @Test
+    public void testNoReadersWithMultipleConcurrentWriters() {
+        List<Thread> threads = Lists.newArrayList();
+        Runnable runnable = new Runnable() {
+
+            @Override
+            public void run() {
+                lock.writeLock().lock();
+            }
+
+        };
+        for (int i = 0; i < TestData.getScaleCount(); i++) {
+            threads.add(new Thread(runnable));
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                Assert.assertFalse(lock.readLock().tryLock());
+            }
+
+        }).start();
+    }
+    
+    @Test
+    public void testNoWritersWithMultipleConcurrentReaders() {
+        List<Thread> threads = Lists.newArrayList();
+        Runnable runnable = new Runnable() {
+
+            @Override
+            public void run() {
+                lock.readLock().lock();
+            }
+
+        };
+        for (int i = 0; i < TestData.getScaleCount(); i++) {
+            threads.add(new Thread(runnable));
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                Assert.assertFalse(lock.writeLock().tryLock());
+            }
+
+        }).start();
+    }
+
+    @Test
+    public void testNotifyWhenReadNoLongerBlocked() {
+        List<Thread> threads = Lists.newArrayList();
+        final AtomicBoolean unlock = new AtomicBoolean(false);
+        Runnable runnable = new Runnable() {
+
+            @Override
+            public void run() {
+                lock.writeLock().lock();
+                while (!unlock.get()) {
+                    continue;
+                }
+                lock.writeLock().unlock();
+            }
+
+        };
+
+        for (int i = 0; i < TestData.getScaleCount(); i++) {
+            threads.add(new Thread(runnable));
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                long t1 = Time.now();
+                lock.readLock().lock();
+                try {
+                    long elapsed = TimeUnit.MILLISECONDS
+                            .convert(Time.now() - t1, TimeUnit.MICROSECONDS);
+                    Assert.assertTrue(elapsed >= (.80 * 100)); // sleep time is
+                                                               // imprecise, so
+                                                               // accept 80%
+                                                               // accuracy
+                }
+                finally {
+                    lock.readLock().unlock();
+                }
+
+            }
+
+        }).start();
+        Threads.sleep(100);
+        unlock.set(true);
+    }
+    
+    @Test
+    public void testNotifyWhenWriteNoLongerBlocked() {
+        List<Thread> threads = Lists.newArrayList();
+        final AtomicBoolean unlock = new AtomicBoolean(false);
+        Runnable runnable = new Runnable() {
+
+            @Override
+            public void run() {
+                lock.readLock().lock();
+                while (!unlock.get()) {
+                    continue;
+                }
+                lock.readLock().unlock();
+            }
+
+        };
+
+        for (int i = 0; i < TestData.getScaleCount(); i++) {
+            threads.add(new Thread(runnable));
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                long t1 = Time.now();
+                lock.writeLock().lock();
+                try {
+                    long elapsed = TimeUnit.MILLISECONDS
+                            .convert(Time.now() - t1, TimeUnit.MICROSECONDS);
+                    Assert.assertTrue(elapsed >= (.80 * 100)); // sleep time is
+                                                               // imprecise, so
+                                                               // accept 80%
+                                                               // accuracy
+                }
+                finally {
+                    lock.writeLock().unlock();
+                }
+
+            }
+
+        }).start();
+        Threads.sleep(100);
+        unlock.set(true);
+    }
+
+}
